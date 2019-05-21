9104ab0969948adde9be582478ac343f335555f5
lichang.zhang <lichang.zhang@ushow.media>
2019-05-21 15:54:30 +0800

    sqoop
    Summary: no
    
    Test Plan: no
    
    Reviewers: zhuohua.chen
    
    Reviewed By: zhuohua.chen
    
    Differential Revision: https://pha.o-pay.in/D1546

--- oride_daily_report/sqoop_db.sh
+++ oride_daily_report/sqoop_db.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-sqoop import --connect "jdbc:mysql://10.52.123.212:3306/oride_data?tinyInt1isBit=false&useUnicode=true&characterEncoding=utf8" \
---username oride \
---password 3r8Kn@483Gh6g3p9 \
+sqoop import --connect "jdbc:mysql://10.52.166.51:13306/oride_data?tinyInt1isBit=false&useUnicode=true&characterEncoding=utf8" \
+--username bigdata \
+--password S5o6KOjEVI \
 --table $1 \
 --target-dir ufile://opay-datalake/oride/db/$1/dt=$2/ \
 --fields-terminated-by "\001" \
