8a0735533376e28166bf981bffe2bfeca3d7a9fd
lichang.zhang <lichang.zhang@ushow.media>
2019-05-31 21:10:18 +0800

    tz_form
    Summary: no
    
    Test Plan: no
    
    Reviewers: zhuohua.chen
    
    Reviewed By: zhuohua.chen
    
    Differential Revision: https://pha.o-pay.in/D2402

--- bad_debts/bad_delts_check.py
+++ bad_debts/bad_delts_check.py
@@ -3,9 +3,12 @@ from utils.connection_helper import get_hive_cursor
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
+import csv
+from email.mime.multipart import MIMEMultipart
 bad_debt_email_limit = 50
 sender = 'research@opay-inc.com'
 password = 'G%4nlD$YCJol@Op'
+# receivers = ['lichang.zhang@opay-inc.com']
 receivers = ['lichang.zhang@opay-inc.com', 'tianzhi.zhang@opay-inc.com']
 
 part_html1 = """<tr>"""
@@ -77,6 +80,101 @@ where dt="{dt}")as b
 on a.id=b.id												
 '''
 
+tmp_query = """
+select 
+a.id,
+a.user_id,
+a.start_name,
+a.end_name,
+a.duration,
+a.distance,
+a.price,
+a.reward,
+a.driver_id,
+a.take_time,
+a.wait_time,
+a.pickup_time,
+a.arrive_time,
+a.finish_time,
+a.cancel_role,
+a.cancel_reason,
+a.cancel_time,
+a.cancel_type,
+a.status,
+a.dt,
+b.id,
+b.driver_id,
+b.mode,
+b.price,
+b.coupon_id,
+b.coupon_amount,
+b.amount,
+b.bonus,
+b.balance,
+b.opay_amount,
+b.reference,
+b.currency,
+b.country,
+b.zfstatus,
+b.modify_time,
+b.create_time,
+b.dt 
+from
+(select 
+id,
+user_id,
+start_name,
+end_name,
+duration,
+distance,
+price,
+reward,
+driver_id,
+from_unixtime(take_time) as take_time,
+from_unixtime(wait_time) as wait_time,
+from_unixtime(pickup_time)as pickup_time,
+from_unixtime(arrive_time)as arrive_time,
+from_unixtime(finish_time)as finish_time,
+cancel_role,
+cancel_reason,
+from_unixtime(cancel_time) as cancel_time,
+cancel_type,
+status,
+dt
+from oride_source.db_data_order
+where dt ="{dt}" and take_time between unix_timestamp('{dt} 00:00:00')and unix_timestamp('{dt} 23:59:59')and status='4')a
+left join 
+(select 
+id,
+driver_id,
+case 
+when mode='0'then '未知'
+when mode='1'then '线下支付'
+when mode='2'then 'opay'
+when mode='3'then '余额'
+end as mode,
+price,
+coupon_id,
+coupon_amount,
+amount,
+bonus,
+balance,
+opay_amount,
+reference,
+currency,
+country,
+case when status='0' then '支付中'
+when status='1' then '成功'
+when status='2' then '失败'
+end as zfstatus,
+modify_time,
+from_unixtime(create_time) as create_time,
+dt 
+from oride_source.db_data_order_payment
+where dt="{dt}" and status<>'1')as b
+on a.id=b.id
+"""
+
 
 def query_repair_table(sql):
     cursor = get_hive_cursor()
@@ -99,6 +197,19 @@ def build_table(data, t_head):
         t_head += part_html3
     t_head += table_tail
     return t_head
+
+
+def build_csv(dt):
+    col_name = ['a.id', 'a.user_id', 'a.start_name', 'a.end_name', 'a.duration', 'a.distance', 'a.price', 'a.reward', 'a.driver_id', 'a.take_time', 'a.wait_time', 'a.pickup_time', 'a.arrive_time', 'a.finish_time', 'a.cancel_role', 'a.cancel_reason', 'a.cancel_time', 'a.cancel_type', 'a.status', 'a.dt', 'b.id', 'b.driver_id', 'b.mode', 'b.price', 'b.coupon_id', 'b.coupon_amount', 'b.amount', 'b.bonus', 'b.balance', 'b.opay_amount', 'b.reference', 'b.currency', 'b.country', 'b.zfstatus', 'b.modify_time', 'b.create_time', 'b.dt']
+    cursor = get_hive_cursor()
+    cursor.execute(tmp_query.format(dt=dt))
+    res = cursor.fetchall()
+    with open("/tmp/tainzhi_query_%s.csv" % dt, "w") as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(col_name)
+        for elem in res:
+            csv_writer.writerow(elem)
+    print("tz csv write done")
 
 
 def build_html_txt(data_user, data_driver, dt):
@@ -128,9 +239,14 @@ def build_html_txt(data_user, data_driver, dt):
     h += t2
     h += mail_msg_tail
     h += css_style
-    message = MIMEText(h, 'html', 'utf-8')
+    message = MIMEMultipart()
+    message.attach(MIMEText(h, 'html', 'utf-8'))
     subject = 'Oride {dt} Bad Debts Daily Report'.format(dt=dt)
     message['Subject'] = Header(subject, 'utf-8')
+    att1 = MIMEText(open("/tmp/tainzhi_query_%s.csv" % dt, 'r').read(), 'plain', 'utf-8')
+    att1["Content-Type"] = 'application/octet-stream'
+    att1["Content-Disposition"] = 'attachment; filename="tianzhi_query_%s.csv"' % dt
+    message.attach(att1)
     return message
 
 
@@ -143,6 +259,7 @@ def check_bad_debts_data(**op_kwargs):
     for name in repair_table_names:
         print(name)
         cursor.execute(repair_table_query % name)
+    build_csv(dt)
     cursor.execute(query1.format(dt=dt))
     res1 = cursor.fetchall()
     user_view_bad_debts = {}
