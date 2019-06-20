b41c9b2b26c053a6585c2b94b6ae3f1c58e768af
zhangzhenqian <jasonzhang081114@gmail.com>
2019-06-20 17:46:14 +0800

    运力布控数据添加h3格子id
    Test Plan: online test
    
    Reviewers: nan.li
    
    Reviewed By: nan.li
    
    Maniphest Tasks: T1700
    
    Differential Revision: https://pha.o-pay.in/D3646

--- oride_transportation_capacity_control.py
+++ oride_transportation_capacity_control.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from airflow.operators.hive_operator import HiveOperator
 from airflow.operators.impala_plugin import ImpalaOperator
 from airflow.operators.hive_to_mysql import HiveToMySqlTransfer
+from airflow.operators.mysql_operator import MySqlOperator
 
 args = {
     'owner': 'root',
@@ -31,6 +32,7 @@ create_oride_transportation_capacity_control = HiveOperator(
         CREATE TABLE IF NOT EXISTS oride_transportation_capacity_control (
           dt string,
           hour string,
+          h3_id string,
           today_order_num int,
           today_finished_num int,
           order_num int,
@@ -47,6 +49,7 @@ create_oride_transportation_capacity_control = HiveOperator(
 insert_oride_transportation_capacity_control = HiveOperator(
     task_id='insert_oride_transportation_capacity_control',
     hql="""
+        create temporary function geoToH3Address as 'com.oride.udf.GeoToH3Address';
         -- 删除数据
         INSERT OVERWRITE TABLE oride_transportation_capacity_control
         SELECT
@@ -60,6 +63,8 @@ insert_oride_transportation_capacity_control = HiveOperator(
             select
                 dt,
                 order_id,
+                MAX(struct(`timestamp`, start_location['lat'], start_location['lng'])).col2 AS lat,
+                MAX(struct(`timestamp`, start_location['lat'], start_location['lng'])).col3 AS lng,
                 MAX(struct(`timestamp`, status)).col2 AS status,
                 MAX(struct(`timestamp`, driver_id)).col2 AS driver_id,
                 MAX(struct(`timestamp`, user_id)).col2 AS user_id,
@@ -75,6 +80,7 @@ insert_oride_transportation_capacity_control = HiveOperator(
         SELECT
             td.dt,
             '{{ execution_date.strftime("%H") }}',
+            td.h3_id,
             td.today_order_num,
             td.today_finished_order_num,
             hd.order_num,
@@ -86,18 +92,22 @@ insert_oride_transportation_capacity_control = HiveOperator(
             (
                 SELECT
                     dt,
+                    geoToH3Address(lat, lng,7) as h3_id,
                     count(order_id) as today_order_num,
                     count(if(status=4 or status=5, order_id, null)) as today_finished_order_num
                 FROM
                     order_data
                 GROUP BY
-                    dt
+                    dt,
+                    geoToH3Address(lat, lng,7)
+
             )
             td
             LEFT JOIN
             (
                 SELECT
                     dt,
+                    geoToH3Address(start_location['lat'],start_location['lng'],7) as h3_id,
                     count(distinct(if(status=0, order_id, null))) as order_num,
                     count(distinct(if(status=4 or status=5, order_id, null))) as finished_order_num,
                     count(distinct(if(status=6 and driver_id=0, order_id, null))) as no_take_order_num,
@@ -108,10 +118,19 @@ insert_oride_transportation_capacity_control = HiveOperator(
                 WHERE
                     dt='{{ ds }}' AND hour='{{ execution_date.strftime("%H") }}'
                 GROUP BY
-                    dt
-            ) hd ON hd.dt=td.dt
+                    dt,
+                    geoToH3Address(start_location['lat'],start_location['lng'],7)
+            ) hd ON hd.dt=td.dt and hd.h3_id=td.h3_id
         """,
     schema='dashboard',
+    dag=dag)
+
+clear_mysql_data = MySqlOperator(
+    task_id='clear_mysql_data',
+    sql="""
+        DELETE FROM oride_transportation_capacity_control WHERE dt = '{{ ds }}' AND hour='{{ execution_date.strftime("%H") }}'
+    """,
+    mysql_conn_id='mysql_bi',
     dag=dag)
 
 export_to_mysql = HiveToMySqlTransfer(
@@ -142,3 +161,5 @@ add_partitions >> insert_oride_transportation_capacity_control
 create_oride_transportation_capacity_control >> insert_oride_transportation_capacity_control
 insert_oride_transportation_capacity_control >> refresh_impala
 insert_oride_transportation_capacity_control >> export_to_mysql
+clear_mysql_data >> export_to_mysql
+
