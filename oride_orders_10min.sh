82f810c56406862452094b3ebf22cf1d1cc50917
duo.wu <duo.wu@opay-inc.com>
2019-06-27 21:48:46 +0800

    修改统计时区为拉各斯时区
    概要: 修改统计的时区为拉各斯时区
    
    测试方案: 统计数据时间为拉各斯时间
    
    审查人: zhangzhenqian
    
    Reviewed By: zhangzhenqian
    
    Differential Revision: https://pha.o-pay.in/D4213

--- oride_orders_10min.sh
+++ oride_orders_10min.sh
@@ -23,7 +23,7 @@ while read day hour orders orders_user orders_pick; do
         values ('${day}', ${hour}, ${orders}, ${orders_user}, ${orders_pick})
             on duplicate key update orders=values(orders), orders_user=values(orders_user), orders_pick=values(orders_pick)"
 done <<_eof
-    $(mysql -h${HOST_RD} -u${USER_RD} -P${PORT_RD} -p${PASS_RD} oride_data --skip-column-names -e"select date_format(from_unixtime(create_time), '%Y-%m-%d') as order_time, date_format(from_unixtime(create_time), '%k') as order_hour, count(1), count(distinct user_id), sum(if(driver_id>0,1,0)) from oride_data.data_order where create_time>=unix_timestamp(date_format(now(),'%Y-%m-%d %H'))-3600 group by order_time, order_hour")
+    $(mysql -h${HOST_RD} -u${USER_RD} -P${PORT_RD} -p${PASS_RD} oride_data --skip-column-names -e"set time_zone = '+1:00'; select date_format(from_unixtime(create_time), '%Y-%m-%d') as order_time, date_format(from_unixtime(create_time), '%k') as order_hour, count(1), count(distinct user_id), sum(if(driver_id>0,1,0)) from oride_data.data_order where create_time>=unix_timestamp(date_format(now(),'%Y-%m-%d %H'))-3600 group by order_time, order_hour")
 _eof
 
 while read day hour drivers; do
@@ -32,5 +32,5 @@ while read day hour drivers; do
     values ('${day}', ${hour}, ${drivers})
     on duplicate key update drivers_serv=values(drivers_serv)"
 done<<_feof
-    $(mysql  -h${HOST_RD} -u${USER_RD} -P${PORT_RD} -p${PASS_RD} oride_data --skip-column-names -e"select date_format(now(), '%Y-%m-%d'), date_format(now(), '%k'), count(1) from data_driver_extend where serv_mode>0")
+    $(mysql  -h${HOST_RD} -u${USER_RD} -P${PORT_RD} -p${PASS_RD} oride_data --skip-column-names -e"set time_zone = '+1:00'; select date_format(now(), '%Y-%m-%d'), date_format(now(), '%k'), count(1) from data_driver_extend where serv_mode>0")
 _feof
