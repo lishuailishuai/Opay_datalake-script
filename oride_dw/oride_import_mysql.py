6e0fa7454e46ea36fffaebaa98a39b08efd933cd
李楠 <lee@linandeMacBook-Pro-2.local>
2019-07-29 20:04:10 +0800

    # Enter a commit message. 调整运行时间
    测试方案: none
    
    审查人: zhangzhenqian
    
    Reviewed By: zhangzhenqian
    
    Differential Revision: https://pha.o-pay.in/D7287

--- oride_dw/oride_import_mysql.py
+++ oride_dw/oride_import_mysql.py
@@ -22,7 +22,7 @@ args = {
 
 dag = airflow.DAG(
     'oride_import_mysql',
-    schedule_interval="04 00 * * *",
+    schedule_interval="00 01 * * *",
     concurrency=5,
     max_active_runs=1,
     default_args=args)
