68b3c3c6bf364bb5597732f4efc93c804bab33c1
nan.li <nan.li@opay-inc.com>
2019-08-23 14:17:13 +0800

    # Enter a commit message. 修改assign日志解析distance未分割问题
    测试方案: online_test
    
    审查人: zhangzhenqian, duo.wu
    
    Reviewed By: zhangzhenqian
    
    Differential Revision: https://pha.static.kunlun.com/D9825

--- oride_dw_code/dwd/dwd_oride_order_assign_driver_detail_di.py
+++ oride_dw_code/dwd/dwd_oride_order_assign_driver_detail_di.py
@@ -22,21 +22,20 @@ import requests
 import os
 
 args = {
-        'owner': 'yangmingze',
-        'start_date': datetime(2019, 5, 20),
-        'depends_on_past': False,
-        'retries': 3,
-        'retry_delay': timedelta(minutes=2),
-        'email': ['bigdata_dw@opay-inc.com'],
-        'email_on_failure': True,
-        'email_on_retry': False,
-} 
+    'owner': 'yangmingze',
+    'start_date': datetime(2019, 5, 20),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=2),
+    'email': ['bigdata_dw@opay-inc.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+}
 
-dag = airflow.DAG( 'dwd_oride_order_assign_driver_detail_di', 
-    schedule_interval="00 01 * * *", 
-    default_args=args,
-    catchup=False) 
-
+dag = airflow.DAG('dwd_oride_order_assign_driver_detail_di',
+                  schedule_interval="00 01 * * *",
+                  default_args=args,
+                  catchup=False)
 
 sleep_time = BashOperator(
     task_id='sleep_id',
@@ -47,59 +46,88 @@ sleep_time = BashOperator(
 ##----------------------------------------- 依赖 ---------------------------------------## 
 
 
-#依赖前一天分区
-dependence_dwd_oride_order_assign_driver_detail_di_prev_day_tesk=HivePartitionSensor(
-      task_id="dwd_oride_order_assign_driver_detail_di_prev_day_task",
-      table="dispatch_tracker_server_magic",
-      partition="dt='{{macros.ds_add(ds, +1)}}' and hour='00'",
-      schema="oride_source",
-      poke_interval=60, #依赖不满足时，一分钟检查一次依赖状态
-      dag=dag
-    )
+# 依赖前一天分区
+dependence_dwd_oride_order_assign_driver_detail_di_prev_day_task = HivePartitionSensor(
+    task_id="dwd_oride_order_assign_driver_detail_di_prev_day_task",
+    table="dispatch_tracker_server_magic",
+    partition="dt='{{macros.ds_add(ds, +1)}}' and hour='00'",
+    schema="oride_source",
+    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
+    dag=dag
+)
 
 ##----------------------------------------- 变量 ---------------------------------------## 
 
 
-table_name="dwd_oride_order_assign_driver_detail_di"
-hdfs_path="ufile://opay-datalake/oride/oride_dw/"+table_name
+table_name = "dwd_oride_order_assign_driver_detail_di"
+hdfs_path = "ufile://opay-datalake/oride/oride_dw/" + table_name
 
 ##----------------------------------------- 脚本 ---------------------------------------## 
 
 dwd_oride_order_assign_driver_detail_di_task = HiveOperator(
-task_id='dwd_oride_order_assign_driver_detail_di_task',
+    task_id='dwd_oride_order_assign_driver_detail_di_task',
 
-hql='''
-SET hive.exec.parallel=TRUE;
-SET hive.exec.dynamic.partition.mode=nonstrict;
-
-insert overwrite table oride_dw.{table} partition(country_code,dt)
+    hql='''
+        SET hive.exec.parallel=TRUE;
+        SET hive.exec.dynamic.partition.mode=nonstrict;
+        
+        insert overwrite table oride_dw.{table} partition(country_code,dt)
+        
         select 
-                get_json_object(event_values, '$.city_id') as city_id,--下单时所在城市
-                get_json_object(event_values, '$.order_id') as order_id, --订单ID
-                get_json_object(event_values, '$.user_id') as passenger_id, --乘客ID
-                driver_id,--司机ID
-                get_json_object(event_values, '$.round') as order_round,--订单轮数
-                get_json_object(event_values, '$.config_id') as config_id,--派单配置的id
-                get_json_object(event_values, '$.success') as success,--是否成功（1，0）
-                get_json_object(event_values, '$.timestamp') as log_timestamp,--埋点时间
-                get_json_object(event_values, '$.distances') as distance,--司机的接驾距离(米)(订单分配给司机时司机所处的位置)
-                get_json_object(event_values, '$.wait_time') as wait_time,--司机收到推送信息时有多久没有订单
-                get_json_object(event_values, '$.mode') as push_mode, --是派单方式（目前只有全局优化和直接发单）
-                'nal' as country_code,
-                dt
-        from  
-        oride_source.dispatch_tracker_server_magic 
-        lateral view explode(split(substr(get_json_object(event_values, '$.driver_ids'),2,length(get_json_object(event_values, '$.driver_ids'))-2),',')) driver_ids as driver_id
-        where  dt = '{pt}' and event_name='dispatch_assign_driver'
+        d.city_id ,--下单时所在城市
+        d.order_id, --订单ID
+        d.passenger_id,--乘客ID
+        d.driver_id,--司机ID
+        d.order_round,--订单轮数
+        d.config_id,--派单配置的id
+        d.success,--是否成功（1，0）
+        d.log_timestamp,--埋点时间
+        d.dis,--司机的接驾距离(米)(订单分配给司机时司机所处的位置)
+        d.wait_time,--司机收到推送信息时有多久没有订单
+        d.push_mode,--是派单方式（目前只有全局优化和直接发单）
+        'nal' as country_code, 
+        '{pt}' dt
+        
+        from 
+        (
+            select
+            *
+            from 
+            (
+                select 
+                split(replace(replace(get_json_object(event_values, '$.driver_ids'),'[',''),']',''), ',') as drivers, 
+                split(replace(replace(get_json_object(event_values, '$.distances'),'[',''),']',''), ',') as distances,
+                get_json_object(event_values, '$.order_id') as order_id,
+                get_json_object(event_values, '$.city_id') as city_id,
+                get_json_object(event_values, '$.user_id') as passenger_id,
+                get_json_object(event_values, '$.round') as order_round,
+                get_json_object(event_values, '$.config_id') as config_id,
+                get_json_object(event_values, '$.success') as success,
+                get_json_object(event_values, '$.timestamp') as log_timestamp,
+                get_json_object(event_values, '$.wait_time') as wait_time,
+                get_json_object(event_values, '$.mode') as push_mode
+                
+                from oride_source.dispatch_tracker_server_magic 
+                where dt='{pt}' and 
+                event_name='dispatch_assign_driver' and 
+                from_unixtime(cast(get_json_object(event_values, '$.timestamp') as int), 'yyyy-MM-dd')='{pt}'
+            ) as t 
+            lateral view posexplode(drivers) d as dpos, driver_id 
+            lateral view posexplode(distances) ds as dspos, dis 
+            where dpos = dspos
+        ) d
+        
+        
+        
 '''.format(
         pt='{{ds}}',
         now_day='{{macros.ds_add(ds, +1)}}',
         table=table_name
-        ),
+    ),
     dag=dag
-    )
+)
 
-#生成_SUCCESS
+# 生成_SUCCESS
 touchz_data_success = BashOperator(
 
     task_id='touchz_data_success',
@@ -118,9 +146,8 @@ touchz_data_success = BashOperator(
     """.format(
         pt='{{ds}}',
         now_day='{{macros.ds_add(ds, +1)}}',
-        hdfs_data_dir=hdfs_path+'/country_code=nal/dt={{ds}}'
-        ),
+        hdfs_data_dir=hdfs_path + '/country_code=nal/dt={{ds}}'
+    ),
     dag=dag)
- 
 
-dependence_dwd_oride_order_assign_driver_detail_di_prev_day_tesk>>sleep_time>>dwd_oride_order_assign_driver_detail_di_task>>touchz_data_success
+dependence_dwd_oride_order_assign_driver_detail_di_prev_day_task >> sleep_time >> dwd_oride_order_assign_driver_detail_di_task >> touchz_data_success
