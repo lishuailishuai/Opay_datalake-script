e8e4c98102a911994d7cf336e08dee210c308913
chenlili <lili.chen@opay-inc.com>
2019-08-31 15:52:17 +0800

    订单全量表接入oride_dwd
    Summary: y
    
    Test Plan: no
    
    Reviewers: nan.li
    
    Reviewed By: nan.li
    
    Differential Revision: https://pha.static.kunlun.com/D10492

--- oride_dw_code/dwd/dwd_oride_order_base_include_test_df.py
+++ oride_dw_code/dwd/dwd_oride_order_base_include_test_df.py
@@ -1,25 +1,14 @@
 # -*- coding: utf-8 -*-
-import airflow
-from datetime import datetime, timedelta
-from airflow.operators.hive_operator import HiveOperator
-from airflow.operators.impala_plugin import ImpalaOperator
-from utils.connection_helper import get_hive_cursor
-from airflow.operators.python_operator import PythonOperator
-from airflow.contrib.hooks.redis_hook import RedisHook
-from airflow.hooks.hive_hooks import HiveCliHook
-from airflow.operators.hive_to_mysql import HiveToMySqlTransfer
-from airflow.operators.mysql_operator import MySqlOperator
-from airflow.operators.dagrun_operator import TriggerDagRunOperator
-from airflow.sensors.external_task_sensor import ExternalTaskSensor
-from airflow.operators.bash_operator import BashOperator
-from airflow.sensors.named_hive_partition_sensor import NamedHivePartitionSensor
-from airflow.sensors.hive_partition_sensor import HivePartitionSensor
-from airflow.sensors import UFileSensor
-import json
 import logging
-from airflow.models import Variable
-import requests
-import os
+from datetime import datetime, timedelta
+
+import airflow
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.hive_operator import HiveOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.sensors.hive_partition_sensor import HivePartitionSensor
+
+from utils.connection_helper import get_hive_cursor
 
 args = {
     'owner': 'chenlili',
@@ -45,45 +34,26 @@ sleep_time = BashOperator(
 
 ##----------------------------------------- 依赖 ---------------------------------------##
 
-# 依赖前一天分区23点
+# 依赖前一天分区
 ods_sqoop_base_data_order_df_prev_day_task = HivePartitionSensor(
     task_id="ods_sqoop_base_data_order_df_prev_day_task",
     table="ods_sqoop_base_data_order_df",
-    partition="dt='{{ds}}' and hour='23'",
+    partition="dt='{{ds}}'",
     schema="oride_dw",
     poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
     dag=dag
 )
 
-# 依赖当天分区00点
-ods_sqoop_base_data_order_df_now_day_task = HivePartitionSensor(
-    task_id="ods_sqoop_base_data_order_df_now_day_task",
-    table="ods_sqoop_base_data_order_df",
-    partition="dt='{{macros.ds_add(ds, +1)}}' and hour='00'",
-    schema="oride_dw",
-    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
-    dag=dag
-)
-
-# 依赖前一天分区23点
-ods_sqoop_base_data_order_payment_df_pre_day_task = HivePartitionSensor(
-    task_id="ods_sqoop_base_data_order_payment_df_pre_day_task",
+# 依赖前一天分区
+ods_sqoop_base_data_order_payment_df_prev_day_task = HivePartitionSensor(
+    task_id="ods_sqoop_base_data_order_payment_df_prev_day_task",
     table="ods_sqoop_base_data_order_payment_df",
-    partition="dt='{{ds}}' and hour='23'",
+    partition="dt='{{ds}}'",
     schema="oride_dw",
     poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
     dag=dag
 )
 
-# 依赖当天分区00点
-ods_sqoop_base_data_order_payment_df_now_day_task = HivePartitionSensor(
-    task_id="ods_sqoop_base_data_order_payment_df_now_day_task",
-    table="ods_sqoop_base_data_order_payment_df",
-    partition="dt='{{macros.ds_add(ds, +1)}}' and hour='00'",
-    schema="oride_dw",
-    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
-    dag=dag
-)
 
 ##----------------------------------------- 变量 ---------------------------------------##
 
@@ -92,7 +62,7 @@ hdfs_path = "ufile://opay-datalake/oride/oride_dw/" + table_name
 
 ##----------------------------------------- 脚本 ---------------------------------------##
 
-dwd_oride_order_base_include_test_di_task = HiveOperator(
+dwd_oride_order_base_include_test_df_task = HiveOperator(
     task_id='dwd_oride_order_base_include_test_df_task',
 
     hql='''
@@ -206,6 +176,7 @@ LEFT OUTER JOIN
         now_day='{{macros.ds_add(ds, +1)}}',
         table=table_name
     ),
+    schema='oride_dw',
     dag=dag
 )
 
@@ -270,10 +241,8 @@ touchz_data_success = BashOperator(
     dag=dag)
 
 ods_sqoop_base_data_order_df_prev_day_task >> \
-ods_sqoop_base_data_order_df_now_day_task >> \
-ods_sqoop_base_data_order_payment_df_pre_day_task >> \
-ods_sqoop_base_data_order_payment_df_now_day_task >> \
+ods_sqoop_base_data_order_payment_df_prev_day_task >> \
 sleep_time >> \
-dwd_oride_order_base_include_test_di_task >> \
+dwd_oride_order_base_include_test_df_task >> \
 task_check_key_data >> \
 touchz_data_success
