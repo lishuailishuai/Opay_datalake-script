93e476fd68f8bfa639c403527fcb48d7c424ded8
杨明泽 <mingze.yang@opay-inc.com>
2019-09-17 19:25:11 +0800

    no message

--- ofood_dw_code/ods/ods_log_client_event_hi.py
+++ ofood_dw_code/ods/ods_log_client_event_hi.py
@@ -18,6 +18,7 @@ dag = airflow.DAG(
     'ods_log_client_event_hi',
     schedule_interval="15 * * * *",
     concurrency=15,
+    max_threads=20,
     default_args=args)
 
 create_ods_log_client_event_hi = HiveOperator(
