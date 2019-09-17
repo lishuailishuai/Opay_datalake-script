a4fa171dbf02f6c97a06fd8b6bd9284ce00ec1b6
杨明泽 <mingze.yang@opay-inc.com>
2019-09-17 20:13:36 +0800

    no message

--- ofood_dw_code/ods/ods_log_client_event_hi.py
+++ ofood_dw_code/ods/ods_log_client_event_hi.py
@@ -57,4 +57,6 @@ ods_log_client_event_hi_partition = HiveOperator(
     dag=dag)
 
 
-create_ods_log_client_event_hi >> ods_log_client_event_hi_partition
+#create_ods_log_client_event_hi >> ods_log_client_event_hi_partition
+
+ods_log_client_event_hi_partition
