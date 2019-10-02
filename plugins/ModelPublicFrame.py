e94f1805fe7510bcc8ccddd0ee79b0ae9cd1ebf6
杨明泽 <mingze.yang@opay-inc.com>
2019-10-02 13:29:41 +0800

    no message

--- plugins/ModelPublicFrame.py
+++ plugins/ModelPublicFrame.py
@@ -25,17 +25,29 @@ class ModelPublicFrame(object):
     hive_cursor = None
     comwx = None
 
-    def __init__(self,execution_date):
+    def __init__(self):
         self.hive_cursor = get_hive_cursor()
         self.comwx = ComwxApi('wwd26d45f97ea74ad2', 'BLE_v25zCmnZaFUgum93j3zVBDK-DjtRkLisI_Wns4g', '1000011')
-        self.ds_date=execution_date.strftime("%Y-%m-%d") #日期(%Y-%m-%d)
-        self.ds_date_hour=execution_date.strftime("%Y-%m-%d %H") #日期(%Y-%m-%d %H)
-        self.ds_date_minute=execution_date.strftime("%Y-%m-%d %H:%M") #日期(%Y-%m-%d %H:%M)
-        self.ds_date_second=execution_date.strftime("%Y-%m-%d %H:%M:%S") #日期(%Y-%m-%d %H:%M:%S)
+
+        self.ds_date=None
+        self.ds_date_hour=None
+        self.ds_date_minute=None
+        self.ds_date_second=None
+
+        self.get_init_date()
+        
 
     def __del__(self):
         self.hive_cursor.close()
         self.hive_cursor = None
+
+
+    def get_init_date(self,execution_date):
+
+        self.ds_date=execution_date.strftime("%Y-%m-%d") #日期(%Y-%m-%d)
+        self.ds_date_hour=execution_date.strftime("%Y-%m-%d %H") #日期(%Y-%m-%d %H)
+        self.ds_date_minute=execution_date.strftime("%Y-%m-%d %H:%M") #日期(%Y-%m-%d %H:%M)
+        self.ds_date_second=execution_date.strftime("%Y-%m-%d %H:%M:%S") #日期(%Y-%m-%d %H:%M:%S)
 
 
     #读取hive location地址
