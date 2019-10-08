df502eb5054e1f7c6758e6f097bdeaf5cbfd3b52
杨明泽 <mingze.yang@opay-inc.com>
2019-10-08 15:30:13 +0800

    no message

--- plugins/ModelPublicFrame.py
+++ plugins/ModelPublicFrame.py
@@ -271,7 +271,7 @@ class ModelPublicFrame(object):
     [{"db":"db_name", "table":"table_name", "partitions":"country_code=nal"]
     """
 
-    def tesk_dependence(self,tables,ds,dag,**op_kwargs):
+    def tesk_dependence(self,tables):
 
         dependence=[]
 
@@ -303,10 +303,10 @@ class ModelPublicFrame(object):
                     dependence_task_flag= HivePartitionSensor(
                         task_id='dependence_{task_id_name}'.format(task_id_name=task_id_flag),
                         table=table,
-                        partition="dt='{pt}'".format(pt=ds),
+                        partition="dt='{pt}'".format(pt=self.ds_date),
                         schema=db,
                         poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
-                        dag=dag
+                        dag=self.dag
                     )
         
                 else:
@@ -317,15 +317,14 @@ class ModelPublicFrame(object):
                         filepath='{hdfs_path_name}/{partition_name}/dt={pt}/_SUCCESS'.format(
                             hdfs_path_name=location,
                             partition_name=partition,
-                            pt=ds
+                            pt=self.ds_date
                         ),
                         bucket_name='opay-datalake',
                         poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
-                        dag=dag
+                        dag=self.dag
                         )
 
                 dependence.append(dependence_task_flag)
-
 
             return dependence
 
