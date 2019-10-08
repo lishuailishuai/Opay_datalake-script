1dfa0ae21edba43ec4ef4817cd79f2e3be013ad9
杨明泽 <mingze.yang@opay-inc.com>
2019-10-08 19:42:34 +0800

    no message

--- plugins/ModelPublicFrame.py
+++ plugins/ModelPublicFrame.py
@@ -268,7 +268,7 @@ class ModelPublicFrame(object):
     [{"db":"db_name", "table":"table_name", "partitions":"country_code=nal"]
     """
 
-    def tesk_dependence(self,tables,dag,ds):
+    def tesk_dependence(self,tables,dag):
 
         dependence=[]
 
@@ -302,7 +302,8 @@ class ModelPublicFrame(object):
                     dependence_task_flag= HivePartitionSensor(
                         task_id='dependence_{task_id_name}'.format(task_id_name=task_id_flag),
                         table=table,
-                        partition="dt='{pt}'".format(pt=ds),
+                        #partition="dt='{pt}'".format(pt=ds),
+                        partition="dt='{{ds}}'",
                         schema=db,
                         poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
                         dag=dag
@@ -313,10 +314,9 @@ class ModelPublicFrame(object):
                     # 配置依赖关系(前一天分区)
                     dependence_task_flag = UFileSensor(
                         task_id='dependence_{task_id_name}'.format(task_id_name=task_id_flag),
-                        filepath='{hdfs_path_name}/{partition_name}/dt={pt}/_SUCCESS'.format(
+                        filepath='{hdfs_path_name}/{partition_name}/dt={{{{ds}}}}/_SUCCESS'.format(
                             hdfs_path_name=location,
-                            partition_name=partition,
-                            pt=ds
+                            partition_name=partition
                         ),
                         bucket_name='opay-datalake',
                         poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
