cc03b067636a3dcc9c027833ec2e58be6ff165b0
杨明泽 <mingze.yang@opay-inc.com>
2019-10-23 21:37:52 +0800

    no message

---
+++ oride_dw_code/dwd/dwd_oride_driver_timerange_di.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+import airflow
+from datetime import datetime, timedelta
+from airflow.operators.hive_operator import HiveOperator
+from airflow.operators.impala_plugin import ImpalaOperator
+from utils.connection_helper import get_hive_cursor
+from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.hooks.redis_hook import RedisHook
+from airflow.hooks.hive_hooks import HiveCliHook
+from airflow.operators.hive_to_mysql import HiveToMySqlTransfer
+from airflow.operators.mysql_operator import MySqlOperator
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
+from airflow.operators.bash_operator import BashOperator
+from airflow.sensors.named_hive_partition_sensor import NamedHivePartitionSensor
+from airflow.sensors.hive_partition_sensor import HivePartitionSensor
+from airflow.sensors import UFileSensor
+import json
+import logging
+from airflow.models import Variable
+import requests
+import os
+
+args = {
+    'owner': 'yangmingze',
+    'start_date': datetime(2019, 10, 1),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=2),
+    'email': ['bigdata_dw@opay-inc.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+}
+
+dag = airflow.DAG('dwd_oride_driver_timerange_di',
+                  schedule_interval="30 * * * *",
+                  default_args=args,
+                  catchup=False)
+
+sleep_time = BashOperator(
+    task_id='sleep_id',
+    depends_on_past=False,
+    bash_command='sleep 10',
+    dag=dag)
+
+##----------------------------------------- 依赖 ---------------------------------------##
+
+
+# 依赖前一天分区
+ods_log_oride_driver_timerange_task = UFileSensor(
+    task_id='ods_log_oride_driver_timerange_task',
+    filepath='{hdfs_path_str}/dt={pt}/_SUCCESS'.format(
+        hdfs_path_str="oride/oride_dw_ods/ods_log_oride_driver_timerange",
+        pt='{{ds}}'
+    ),
+    bucket_name='opay-datalake',
+    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
+    dag=dag
+) 
+
+##----------------------------------------- 变量 ---------------------------------------##
+
+
+table_name = "dwd_oride_driver_timerange_di"
+hdfs_path = "ufile://opay-datalake/oride/oride_dw/" + table_name
+
+##----------------------------------------- 脚本 ---------------------------------------##
+
+dwd_oride_driver_timerange_di_task = HiveOperator(
+    task_id='dwd_oride_driver_timerange_di_task',
+
+    hql='''
+        SET hive.exec.parallel=TRUE;
+        SET hive.exec.dynamic.partition.mode=nonstrict;
+
+        insert overwrite table oride_dw.{table} partition(country_code,dt)
+
+        SELECT
+            driver_id,
+            driver_onlinerange,
+            driver_freerange,
+            'nal' as country_code,
+            '{pt}' as dt
+        FROM
+            oride_dw_ods.ods_log_oride_driver_timerange
+        WHERE
+            dt='{pt}'
+        ;
+'''.format(
+        pt='{{ds}}',
+        now_hour='{{ execution_date.strftime("%H") }}',
+        table=table_name
+    ),
+    dag=dag
+)
+
+#生成_SUCCESS
+def check_success(ds,dag,**op_kwargs):
+
+    dag_ids=dag.dag_id
+
+    msg = [
+        {"table":"{dag_name}".format(dag_name=dag_ids),"hdfs_path": "{hdfsPath}/country_code=nal/dt={pt}".format(pt=ds,hdfsPath=hdfs_path)}
+    ]
+
+    TaskTouchzSuccess().set_touchz_success(msg)
+
+touchz_data_success= PythonOperator(
+    task_id='touchz_data_success',
+    python_callable=check_success,
+    provide_context=True,
+    dag=dag
+)
+
+ods_log_oride_driver_timerange_task >> sleep_time >> dwd_oride_driver_timerange_di_task >> touchz_data_success
