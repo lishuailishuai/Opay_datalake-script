aceaeb8ab28d8d8ed8fc80cf38e9e20ed46ea79c
nan.li <nan.li@opay-inc.com>
2019-10-25 17:32:02 +0800

    修改导入表

--- opos_dw/opos_source_sqoop_di.py
+++ opos_dw/opos_source_sqoop_di.py
@@ -12,7 +12,6 @@ from plugins.SqoopSchemaUpdate import SqoopSchemaUpdate
 from plugins.TaskTimeoutMonitor import TaskTimeoutMonitor
 from utils.util import on_success_callback
 
-
 args = {
     'owner': 'linan',
     'start_date': datetime(2019, 10, 20),
@@ -22,7 +21,7 @@ args = {
     'email': ['bigdata_dw@opay-inc.com'],
     'email_on_failure': True,
     'email_on_retry': False,
-    'on_success_callback':on_success_callback,
+    'on_success_callback': on_success_callback,
 }
 
 dag = airflow.DAG(
@@ -32,15 +31,15 @@ dag = airflow.DAG(
     max_active_runs=1,
     default_args=args)
 
+
 ##----------------------------------------- 任务超时监控 ---------------------------------------##
 
 def fun_task_timeout_monitor(ds, db_name, table_name, **op_kwargs):
     tb = [
-        {"db": db_name, "table":table_name, "partition": "dt={pt}".format(pt=ds), "timeout": "7200"}
+        {"db": db_name, "table": table_name, "partition": "dt={pt}".format(pt=ds), "timeout": "7200"}
     ]
 
     TaskTimeoutMonitor().set_task_monitor(tb)
-
 
 
 '''
@@ -50,12 +49,12 @@ db_name,table_name,conn_id,prefix_name,priority_weight
 #
 
 table_list = [
-    ("ptsp_db", "transactions", "opos_ptsp_db", "base",3),
+    ("ptsp_db", "opos_payment_order", "opos_ptsp_db", "base", 3, True),
 ]
 
 HIVE_DB = 'opos_dw_ods'
 HIVE_TABLE = 'ods_sqoop_%s_%s_di'
-UFILE_PATH = 'ufile://opay-datalake/oride_dw_sqoop_di/%s/%s'
+UFILE_PATH = 'ufile://opay-datalake/opos_dw_sqoop_di/%s/%s'
 ODS_CREATE_TABLE_SQL = '''
     CREATE EXTERNAL TABLE IF NOT EXISTS {db_name}.`{table_name}`(
         {columns}
@@ -83,78 +82,23 @@ table_dim_list = []
 # 需要验证的非核心业务表，根据需求陆续添加
 table_not_core_list = []
 
-
-def run_check_table(db_name, table_name, conn_id, hive_table_name, **kwargs):
-    sqoopSchema = SqoopSchemaUpdate()
-    response = sqoopSchema.update_hive_schema(
-        hive_db=HIVE_DB,
-        hive_table=hive_table_name,
-        mysql_db=db_name,
-        mysql_table=table_name,
-        mysql_conn=conn_id
-    )
-    if response:
-        return True
-
-    # SHOW TABLES in oride_db LIKE 'data_aa'
-    check_sql = 'SHOW TABLES in %s LIKE \'%s\'' % (HIVE_DB, hive_table_name)
-    hive2_conn = HiveServer2Hook().get_conn()
-    cursor = hive2_conn.cursor()
-    cursor.execute(check_sql)
-    if len(cursor.fetchall()) == 0:
-        logging.info('Create Hive Table: %s.%s', HIVE_DB, hive_table_name)
-        # get table column
-        column_sql = '''
-            SELECT
-                COLUMN_NAME,
-                DATA_TYPE,
-                NUMERIC_PRECISION,
-                NUMERIC_SCALE,COLUMN_COMMENT
-            FROM
-                information_schema.columns
-            WHERE
-                table_schema='{db_name}' and table_name='{table_name}'
-        '''.format(db_name=db_name, table_name=table_name)
-        mysql_hook = MySqlHook(conn_id)
-        mysql_conn = mysql_hook.get_conn()
-        mysql_cursor = mysql_conn.cursor()
-        mysql_cursor.execute(column_sql)
-        results = mysql_cursor.fetchall()
-        rows = []
-        for result in results:
-            if result[0] == 'dt':
-                col_name = '_dt'
-            else:
-                col_name = result[0]
-            if result[1] == 'timestamp' or result[1] == 'varchar' or result[1] == 'char' or result[1] == 'text' or \
-                    result[1] == 'datetime':
-                data_type = 'string'
-            elif result[1] == 'decimal':
-                data_type = result[1] + "(" + str(result[2]) + "," + str(result[3]) + ")"
-            else:
-                data_type = result[1]
-            rows.append("`%s` %s comment '%s'" % (col_name, data_type, result[4]))
-        mysql_conn.close()
-
-        # hive create table
-        hive_hook = HiveCliHook()
-        sql = ODS_CREATE_TABLE_SQL.format(
-            db_name=HIVE_DB,
-            table_name=hive_table_name,
-            columns=",\n".join(rows),
-            ufile_path=UFILE_PATH % (db_name, table_name)
-        )
-        logging.info('Executing: %s', sql)
-        hive_hook.run_cli(sql)
-    return
-
-
 conn_conf_dict = {}
-for db_name, table_name, conn_id, prefix_name,priority_weight_nm in table_list:
+for db_name, table_name, conn_id, prefix_name, priority_weight_nm, is_time_partition in table_list:
     if conn_id not in conn_conf_dict:
         conn_conf_dict[conn_id] = BaseHook.get_connection(conn_id)
 
     hive_table_name = HIVE_TABLE % (prefix_name, table_name)
+
+    import_table_name = ''
+    if is_time_partition:
+        # 时间分区表特殊处理
+        day = '{{ ds }}'
+        year = datetime.strptime(day, "%Y-%m-%d").strftime("%Y")
+        week = datetime.strptime(day, "%Y-%m-%d").strftime("%W")
+        import_table_name = table_name + '_' + year + '_' + week
+    else:
+        import_table_name = table_name
+
     # sqoop import
     import_table = BashOperator(
         task_id='import_table_{}'.format(hive_table_name),
@@ -166,7 +110,7 @@ for db_name, table_name, conn_id, prefix_name,priority_weight_nm in table_list:
             --connect "jdbc:mysql://{host}:{port}/{schema}?tinyInt1isBit=false&useUnicode=true&characterEncoding=utf8" \
             --username {username} \
             --password {password} \
-            --query 'select * from {table} where (DATE_FORMAT(created,"%Y-%m-%d")="{{{{ ds }}}}" OR DATE_FORMAT(report_time,"%Y-%m-%d")="{{{{ ds }}}}") AND $CONDITIONS' \
+            --query 'select * from {table} where (DATE_FORMAT(create_time,"%Y-%m-%d")="{{{{ ds }}}}" OR DATE_FORMAT(modify_time,"%Y-%m-%d")="{{{{ ds }}}}") AND $CONDITIONS' \
             --split-by id \
             --target-dir {ufile_path}/dt={{{{ ds }}}}/ \
             --fields-terminated-by "\\001" \
@@ -181,26 +125,12 @@ for db_name, table_name, conn_id, prefix_name,priority_weight_nm in table_list:
             schema=db_name,
             username=conn_conf_dict[conn_id].login,
             password=conn_conf_dict[conn_id].password,
-            table=table_name,
+            table=import_table_name,
             ufile_path=UFILE_PATH % (db_name, table_name)
         ),
         dag=dag,
     )
 
-    # check table
-    check_table = PythonOperator(
-        task_id='check_table_{}'.format(hive_table_name),
-        priority_weight=priority_weight_nm,
-        python_callable=run_check_table,
-        provide_context=True,
-        op_kwargs={
-            'db_name': db_name,
-            'table_name': table_name,
-            'conn_id': conn_id,
-            'hive_table_name': hive_table_name
-        },
-        dag=dag
-    )
     # add partitions
     add_partitions = HiveOperator(
         task_id='add_partitions_{}'.format(hive_table_name),
@@ -227,7 +157,7 @@ for db_name, table_name, conn_id, prefix_name,priority_weight_nm in table_list:
     )
 
     # 超时监控
-    task_timeout_monitor= PythonOperator(
+    task_timeout_monitor = PythonOperator(
         task_id='task_timeout_monitor_{}'.format(hive_table_name),
         priority_weight=priority_weight_nm,
         python_callable=fun_task_timeout_monitor,
@@ -239,5 +169,4 @@ for db_name, table_name, conn_id, prefix_name,priority_weight_nm in table_list:
         dag=dag
     )
 
-
-    import_table >> check_table >> add_partitions >> validate_all_data
+    import_table >> add_partitions >> validate_all_data
