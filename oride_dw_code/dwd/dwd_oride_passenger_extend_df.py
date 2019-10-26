acb3cb88c97371d073e06a2ebd53081907668d29
杨明泽 <mingze.yang@opay-inc.com>
2019-10-26 16:22:43 +0800

    no message

---
+++ oride_dw_code/dwd/dwd_oride_passenger_extend_df.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+import airflow
+from datetime import datetime, timedelta
+from airflow.operators.hive_operator import HiveOperator
+from airflow.operators.impala_plugin import ImpalaOperator
+from utils.connection_helper import get_hive_cursor
+from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.hooks.redis_hook import RedisHook
+from airflow.hooks.hive_hooks import HiveCliHook
+from airflow.operators.hive_to_mysql import HiveToMySqlTransfer
+from airflow.operators.mysql_operator import MySqlOperator
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
+from airflow.operators.bash_operator import BashOperator
+from airflow.sensors.named_hive_partition_sensor import NamedHivePartitionSensor
+from airflow.sensors.hive_partition_sensor import HivePartitionSensor
+from airflow.sensors import UFileSensor
+from plugins.TaskTimeoutMonitor import TaskTimeoutMonitor
+from plugins.TaskTouchzSuccess import TaskTouchzSuccess
+import json
+import logging
+from airflow.models import Variable
+import requests
+import os
+
+args = {
+    'owner': 'lijialong',
+    'start_date': datetime(2019, 10, 1),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=2),
+    'email': ['bigdata_dw@opay-inc.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+}
+
+dag = airflow.DAG('dwd_oride_passenger_extend_df',
+                  schedule_interval="20 01 * * *",
+                  default_args=args,
+                  catchup=False)
+
+sleep_time = BashOperator(
+    task_id='sleep_id',
+    depends_on_past=False,
+    bash_command='sleep 10',
+    dag=dag)
+
+##----------------------------------------- 依赖 ---------------------------------------##
+
+
+# 依赖前一天分区
+ods_sqoop_base_data_user_extend_df_task = UFileSensor(
+    task_id='ods_sqoop_base_data_user_extend_df_task',
+    filepath='{hdfs_path_str}/dt={pt}/_SUCCESS'.format(
+        hdfs_path_str="oride_dw_sqoop/oride_data/data_user_extend",
+        pt='{{ds}}'
+    ),
+    bucket_name='opay-datalake',
+    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
+    dag=dag
+)
+
+##----------------------------------------- 变量 ---------------------------------------##
+
+
+table_name = "dwd_oride_passenger_extend_df"
+hdfs_path = "ufile://opay-datalake/oride/oride_dw/" + table_name
+
+##----------------------------------------- 任务超时监控 ---------------------------------------## 
+
+def fun_task_timeout_monitor(ds,dag,**op_kwargs):
+
+    dag_ids=dag.dag_id
+
+    tb = [
+        {"db": "oride_dw", "table":"{dag_name}".format(dag_name=dag_ids), "partition": "country_code=nal/dt={pt}".format(pt=ds), "timeout": "600"}
+    ]
+
+    TaskTimeoutMonitor().set_task_monitor(tb)
+
+task_timeout_monitor= PythonOperator(
+    task_id='task_timeout_monitor',
+    python_callable=fun_task_timeout_monitor,
+    provide_context=True,
+    dag=dag
+)
+
+##----------------------------------------- 脚本 ---------------------------------------##
+
+dwd_oride_passenger_extend_df_task = HiveOperator(
+    task_id='dwd_oride_passenger_extend_df_task',
+
+    hql='''
+        SET hive.exec.parallel=TRUE;
+        SET hive.exec.dynamic.partition.mode=nonstrict;
+
+        insert overwrite table oride_dw.{table} partition(country_code,dt)
+
+        SELECT
+            id as passenger_id,--'用户 ID', 
+            take_order,--'接单数量', 
+            avg_score,--'平均评分', 
+            total_score,--'总评分', 
+            score_times,--'评分次数', 
+            bonus,--'奖励金', 
+            balance,--'余额', 
+            last_order_id,--'最近一个订单的ID', 
+            register_time,--'注册时间', 
+            login_time,--'最后登陆时间', 
+            inviter_role,--'', 
+            inviter_id,--'', 
+            invite_num,--'', 
+            invite_complete_num,--'', 
+            invite_award,--'', 
+            updated_at,--'最后更新时间', 
+            pay_type,--'user auto pay settings(-1: not set 0: manual payment 1: auto payment)', 
+            city_id,--'注册城市', 
+            language,--'客户端语言', 
+            finish_order,--'完单数量', 
+            mark,--'按位通用标记'
+            'nal' as country_code,
+            '{pt}' as dt
+        FROM
+            oride_dw_ods.ods_sqoop_base_data_user_extend_df
+        WHERE
+            dt='{pt}'
+        ;
+'''.format(
+        pt='{{ds}}',
+        now_hour='{{ execution_date.strftime("%H") }}',
+        table=table_name
+    ),
+    dag=dag
+)
+
+#生成_SUCCESS
+def check_success(ds,dag,**op_kwargs):
+
+    dag_ids=dag.dag_id
+
+    msg = [
+        {"table":"{dag_name}".format(dag_name=dag_ids),"hdfs_path": "{hdfsPath}/country_code=nal/dt={pt}".format(pt=ds,hdfsPath=hdfs_path)}
+    ]
+
+    TaskTouchzSuccess().set_touchz_success(msg)
+
+touchz_data_success= PythonOperator(
+    task_id='touchz_data_success',
+    python_callable=check_success,
+    provide_context=True,
+    dag=dag
+)
+
+ods_sqoop_base_data_user_extend_df_task >> sleep_time >> dwd_oride_passenger_extend_df_task >> touchz_data_success
