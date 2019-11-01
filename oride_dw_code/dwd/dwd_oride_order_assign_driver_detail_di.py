17de08e2bda368e69022cfc29f959a653a79d9b0
nan.li <nan.li@opay-inc.com>
2019-11-01 10:18:25 +0800

    修改bug

--- oride_dw_code/dwd/dwd_oride_order_assign_driver_detail_di.py
+++ oride_dw_code/dwd/dwd_oride_order_assign_driver_detail_di.py
@@ -73,7 +73,7 @@ def fun_task_timeout_monitor(ds, dag, **op_kwargs):
 
     tb = [
         {"db": "oride_dw", "table": "{dag_name}".format(dag_name=dag_ids),
-         "partition": "country_code=nal/dt={pt}".format(pt=ds), "timeout": "3000girta"}
+         "partition": "country_code=nal/dt={pt}".format(pt=ds), "timeout": "3000"}
     ]
 
     TaskTimeoutMonitor().set_task_monitor(tb)
