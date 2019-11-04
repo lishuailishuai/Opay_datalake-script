1fd22e26e42f6b81f880c7f0d196f31b47d39903
杨明泽 <mingze.yang@opay-inc.com>
2019-11-04 18:49:42 +0800

    no message

--- oride_dw_code/app/app_oride_finance_driver_repayment_d.py
+++ oride_dw_code/app/app_oride_finance_driver_repayment_d.py
@@ -115,7 +115,10 @@ def get_data_from_hive(ds,**op_kwargs):
             nvl(driver_finish_ord_num,0) AS order_numbers, -- 完成订单数量
             nvl(t3.order_agv,0) AS order_agv, -- 3日平均
             fault,
-            plate_number --车牌号
+            plate_number, --车牌号
+            register_time, -- 司机注册时间
+            driver_address, --司机地址（-1 未知）
+            last_week_daily_due --上周日均应还款金额
         FROM (select * FROM {hive_db}.{hive_table}
         WHERE dt = '{pt}') t1
         
@@ -167,7 +170,7 @@ def get_data_from_hive(ds,**op_kwargs):
         [
             'day', 'city_id', 'city_name', 'driver_id', 'driver_name', 'driver_mobile', 'driver_type',
             'balance', 'repayment_total_amount', 'start_date', 'repayment_amount', 'total_numbers',
-            'effective_days', 'lose_numbers', 'last_back_time', 'today_repayment', 'status','order_numbers','order_agv','fault','plate_number'
+            'effective_days', 'lose_numbers', 'last_back_time', 'today_repayment', 'status','order_numbers','order_agv','fault','plate_number','register_time','driver_address','last_week_daily_due'
         ],
         'day=VALUES(day)'
     )
@@ -203,12 +206,12 @@ def __data_to_mysql(conn, data, column, update=''):
     try:
         for (day, city_id, city_name, driver_id, driver_name, driver_mobile, driver_type,
                 balance, repayment_total_amount, start_date, repayment_amount, total_numbers,
-                effective_days, lose_numbers, last_back_time, today_repayment, status,order_numbers,order_agv,fault,plate_number) in data:
+                effective_days, lose_numbers, last_back_time, today_repayment, status,order_numbers,order_agv,fault,plate_number,register_time,driver_address,last_week_daily_due) in data:
 
             row = [
                 day, city_id, city_name, driver_id, driver_name.replace("'", "\\'"), driver_mobile, driver_type,
                 balance, repayment_total_amount, start_date, repayment_amount, total_numbers,
-                effective_days, lose_numbers, last_back_time, today_repayment, status,order_numbers,order_agv,fault,plate_number
+                effective_days, lose_numbers, last_back_time, today_repayment, status,order_numbers,order_agv,fault,plate_number,register_time,driver_address,last_week_daily_due
             ]
             if sval == '':
                 sval = '(\'{}\')'.format('\',\''.join([str(x) for x in row]))
