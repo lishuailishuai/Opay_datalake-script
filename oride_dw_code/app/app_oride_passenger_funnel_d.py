f749c58bdbcb8f582686dbef99558335560e3bd9
shuzhen.liu <shuzhen.liu@opay-inc.com>
2019-11-07 10:59:49 +0800

    漏斗时间修复
    Summary: 修复
    
    Test Plan: 修复
    
    Reviewers: dong.xie
    
    Reviewed By: dong.xie
    
    Differential Revision: https://pha.static.kunlun.com/D14256

--- oride_dw_code/app/app_oride_passenger_funnel_d.py
+++ oride_dw_code/app/app_oride_passenger_funnel_d.py
@@ -23,7 +23,7 @@ import os
 
 args = {
     'owner': 'liushuzhen',
-    'start_date': datetime(2019, 11, 07),
+    'start_date': datetime(2019, 11, 7),
     'depends_on_past': False,
     'retries': 3,
     'retry_delay': timedelta(minutes=2),
