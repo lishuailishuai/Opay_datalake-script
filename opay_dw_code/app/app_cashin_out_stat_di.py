90e6b76edea1301780d59770620e026bf564b7ed
xiedong <dong.xie@opay-inc.com>
2019-11-08 17:06:38 +0800

    cashin out临时统计

---
+++ opay_dw_code/app/app_cashin_out_stat_di.py
@@ -0,0 +1,132 @@
+import airflow
+from datetime import datetime, timedelta
+from airflow.operators.hive_operator import HiveOperator
+from airflow.operators.impala_plugin import ImpalaOperator
+from utils.connection_helper import get_hive_cursor
+from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.hooks.redis_hook import RedisHook
+from airflow.hooks.hive_hooks import HiveCliHook
+from airflow.operators.hive_to_mysql import HiveToMySqlTransfer
+from airflow.operators.mysql_operator import MySqlOperator
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
+from airflow.operators.bash_operator import BashOperator
+from airflow.sensors.named_hive_partition_sensor import NamedHivePartitionSensor
+from airflow.sensors.hive_partition_sensor import HivePartitionSensor
+from airflow.sensors import UFileSensor
+from plugins.TaskTimeoutMonitor import TaskTimeoutMonitor
+from plugins.TaskTouchzSuccess import TaskTouchzSuccess
+import json
+import logging
+from airflow.models import Variable
+import requests
+import os
+
+##
+# 央行月报汇报指标
+#
+args = {
+    'owner': 'xiedong',
+    'start_date': datetime(2019, 11, 7),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=2),
+    'email': ['bigdata_dw@opay-inc.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+}
+
+
+dag = airflow.DAG('app_cashin_out_stat_di',
+                 schedule_interval="20 03 * * *",
+                  default_args=args,
+                  catchup=False)
+
+##----------------------------------------- 依赖 ---------------------------------------##
+ods_sqoop_base_cash_in_record_di_prev_day_task = UFileSensor(
+    task_id='ods_sqoop_base_cash_in_record_di_prev_day_task',
+    filepath='{hdfs_path_str}/dt={pt}/_SUCCESS'.format(
+        hdfs_path_str="opay_dw_sqoop_di/opay_transaction/cash_in_record",
+        pt='{{ds}}'
+    ),
+    bucket_name='opay-datalake',
+    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
+    dag=dag
+)
+
+ods_sqoop_base_cash_out_record_di_prev_day_task = UFileSensor(
+    task_id='ods_sqoop_base_cash_out_record_di_prev_day_task',
+    filepath='{hdfs_path_str}/dt={pt}/_SUCCESS'.format(
+        hdfs_path_str="opay_dw_sqoop_di/opay_transaction/cash_out_record",
+        pt='{{ds}}'
+    ),
+    bucket_name='opay-datalake',
+    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
+    dag=dag
+)
+
+##----------------------------------------- 任务超时监控 ---------------------------------------##
+def fun_task_timeout_monitor(ds,dag,**op_kwargs):
+
+    dag_ids=dag.dag_id
+
+    msg = [
+        {"db": "opay_dw", "table":"{dag_name}".format(dag_name=dag_ids), "partition": "country_code=NG/dt={pt}".format(pt=ds), "timeout": "3000"}
+    ]
+
+    TaskTimeoutMonitor().set_task_monitor(msg)
+
+task_timeout_monitor= PythonOperator(
+    task_id='task_timeout_monitor',
+    python_callable=fun_task_timeout_monitor,
+    provide_context=True,
+    dag=dag
+)
+
+table_name = "app_cashin_out_stat_di"
+hdfs_path="ufile://opay-datalake/opay/opay_dw/" + table_name
+
+##---- hive operator ---##
+app_cashin_out_stat_di_task = HiveOperator(
+    task_id='app_cashin_out_stat_di_task',
+    hql='''
+    set hive.exec.dynamic.partition.mode=nonstrict;
+    set hive.exec.parallel=true;
+
+    insert overwrite table app_cashin_out_stat_di_task
+    partition(country_count='NG',dt='{pt}')
+    select 
+        sum(amount) order_amt, count(*) order_cnt, 'CashIn' service_type
+    from opay_dw_ods.ods_sqoop_base_cash_in_record_di
+    where dt='{pt}'
+    union all
+    select 
+        sum(amount) order_amt, count(*) order_cnt, 'CashOut' service_type
+    from opay_dw_ods.ods_sqoop_base_cash_out_record_di
+    where dt='{pt}'
+    '''.format(
+        pt='{{ds}}'
+    ),
+    schema='opay_dw',
+    dag=dag
+)
+
+#生成_SUCCESS
+def check_success(ds,dag,**op_kwargs):
+
+    dag_ids=dag.dag_id
+
+    msg = [
+        {"table":"{dag_name}".format(dag_name=dag_ids),"hdfs_path": "{hdfsPath}/country_code=NG/dt={pt}".format(pt=ds,hdfsPath=hdfs_path)}
+    ]
+
+    TaskTouchzSuccess().set_touchz_success(msg)
+
+touchz_data_success= PythonOperator(
+    task_id='touchz_data_success',
+    python_callable=check_success,
+    provide_context=True,
+    dag=dag
+)
+
+app_cashin_out_stat_di_task >> touchz_data_success
