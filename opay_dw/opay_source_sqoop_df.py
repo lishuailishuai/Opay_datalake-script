5f52f83f69c887f448566ceebffc6ee5bc9e8238
杨明泽 <mingze.yang@opay-inc.com>
2019-11-09 10:41:17 +0800

    no message

--- opay_dw/opay_source_sqoop_df.py
+++ opay_dw/opay_source_sqoop_df.py
@@ -23,7 +23,7 @@ args = {
     'email_on_retry': False,
     'on_success_callback':on_success_callback,
 }
-schedule_interval="00 04 * * *"
+schedule_interval="00 05 * * *"
 
 dag = airflow.DAG(
     'opay_source_sqoop_df',
