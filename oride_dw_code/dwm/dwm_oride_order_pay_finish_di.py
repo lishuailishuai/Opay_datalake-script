51b509a4890611c3519ccbe3d578da4eefbc7004
杨明泽 <mingze.yang@opay-inc.com>
2019-11-15 10:49:15 +0800

    no message

--- oride_dw_code/dwm/dwm_oride_order_pay_finish_di.py
+++ oride_dw_code/dwm/dwm_oride_order_pay_finish_di.py
@@ -15,6 +15,8 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.sensors.named_hive_partition_sensor import NamedHivePartitionSensor
 from airflow.sensors.hive_partition_sensor import HivePartitionSensor
 from airflow.sensors import UFileSensor
+from plugins.TaskTimeoutMonitor import TaskTimeoutMonitor
+from plugins.TaskTouchzSuccess import TaskTouchzSuccess
 import json
 import logging
 from airflow.models import Variable
@@ -55,6 +57,25 @@ dwd_oride_order_pay_detail_di_prev_day_tesk=UFileSensor(
 db_name="oride_dw"
 table_name="dwm_oride_order_pay_finish_di"
 hdfs_path="ufile://opay-datalake/oride/oride_dw/"+table_name
+
+##----------------------------------------- 任务超时监控 ---------------------------------------## 
+
+def fun_task_timeout_monitor(ds,dag,**op_kwargs):
+
+    dag_ids=dag.dag_id
+
+    msg = [
+        {"db": "ofood_dw", "table":"{dag_name}".format(dag_name=dag_ids), "partition": "country_code=nal/dt={pt}".format(pt=ds), "timeout": "800"}
+    ]
+
+    TaskTimeoutMonitor().set_task_monitor(msg)
+
+task_timeout_monitor= PythonOperator(
+    task_id='task_timeout_monitor',
+    python_callable=fun_task_timeout_monitor,
+    provide_context=True,
+    dag=dag
+)
 
 ##----------------------------------------- 脚本 ---------------------------------------## 
 
@@ -129,7 +150,7 @@ def execution_data_task_id(ds,**kargs):
     hive_hook.run_cli(_sql)
 
     #熔断数据
-    check_key_data_task(ds)
+    #check_key_data_task(ds)
 
     #生成_SUCCESS
     """
