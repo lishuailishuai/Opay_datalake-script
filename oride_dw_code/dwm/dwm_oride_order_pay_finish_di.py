c82eb923418c408268aa67bc6d6fd77eab852437
杨明泽 <mingze.yang@opay-inc.com>
2019-11-16 09:37:17 +0800

    no message

--- oride_dw_code/dwm/dwm_oride_order_pay_finish_di.py
+++ oride_dw_code/dwm/dwm_oride_order_pay_finish_di.py
@@ -65,7 +65,7 @@ def fun_task_timeout_monitor(ds,dag,**op_kwargs):
     dag_ids=dag.dag_id
 
     msg = [
-        {"db": "ofood_dw", "table":"{dag_name}".format(dag_name=dag_ids), "partition": "country_code=nal/dt={pt}".format(pt=ds), "timeout": "800"}
+        {"db": "oride_dw", "table":"{dag_name}".format(dag_name=dag_ids), "partition": "country_code=nal/dt={pt}".format(pt=ds), "timeout": "800"}
     ]
 
     TaskTimeoutMonitor().set_task_monitor(msg)
