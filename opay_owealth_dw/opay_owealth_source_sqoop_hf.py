fbc6d2478142c6ba69ac87717027aaefaa95a05e
杨明泽 <mingze.yang@opay-inc.com>
2019-11-23 12:40:18 +0800

    no message

--- opay_owealth_dw/opay_owealth_source_sqoop_hf.py
+++ opay_owealth_dw/opay_owealth_source_sqoop_hf.py
@@ -14,7 +14,7 @@ from utils.util import on_success_callback
 
 args = {
     'owner': 'yangmingze',
-    'start_date': datetime(2019, 10, 30),
+    'start_date': datetime(2019, 11, 20),
     'depends_on_past': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
