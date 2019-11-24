0dcd65abcfedaa600898995612718b02648cd743
杨明泽 <mingze.yang@opay-inc.com>
2019-11-24 21:57:53 +0800

    no message

---
+++ oride_dw_code/dim/test_dim_oride_city.py
@@ -0,0 +1,332 @@
+# -*- coding: utf-8 -*-
+import airflow
+from datetime import datetime, timedelta
+from airflow.operators.hive_operator import HiveOperator
+from airflow.operators.impala_plugin import ImpalaOperator
+from utils.connection_helper import get_hive_cursor
+from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.hooks.redis_hook import RedisHook
+from airflow.hooks.hive_hooks import HiveCliHook, HiveServer2Hook
+from airflow.operators.hive_to_mysql import HiveToMySqlTransfer
+from airflow.operators.mysql_operator import MySqlOperator
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
+from airflow.operators.bash_operator import BashOperator
+from airflow.sensors.named_hive_partition_sensor import NamedHivePartitionSensor
+from airflow.sensors.hive_partition_sensor import HivePartitionSensor
+from airflow.sensors import UFileSensor
+from plugins.TaskTimeoutMonitor import TaskTimeoutMonitor
+from plugins.TaskTouchzSuccess import TaskTouchzSuccess
+from plugins.CountriesPublicFrame import CountriesPublicFrame
+import json
+import logging
+from airflow.models import Variable
+import requests
+import os
+
+args = {
+        'owner': 'yangmingze',
+        'start_date': datetime(2019, 5, 20),
+        'depends_on_past': False,
+        'retries': 3,
+        'retry_delay': timedelta(minutes=2),
+        'email': ['bigdata_dw@opay-inc.com'],
+        'email_on_failure': True,
+        'email_on_retry': False,
+} 
+
+dag = airflow.DAG( 'test_dim_oride_city', 
+    schedule_interval="00 01 * * *", 
+    default_args=args,
+    catchup=False) 
+
+
+##----------------------------------------- 依赖 ---------------------------------------## 
+
+
+ods_sqoop_base_data_city_conf_df_tesk = UFileSensor(
+    task_id='ods_sqoop_base_data_city_conf_df_tesk',
+    filepath='{hdfs_path_str}/dt={pt}/_SUCCESS'.format(
+        hdfs_path_str="oride_dw_sqoop/oride_data/data_city_conf",
+        pt='{{ds}}'
+    ),
+    bucket_name='opay-datalake',
+    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
+    dag=dag
+)
+
+ods_sqoop_base_data_country_conf_df_tesk = UFileSensor(
+    task_id='ods_sqoop_base_data_country_conf_df_tesk',
+    filepath='{hdfs_path_str}/dt={pt}/_SUCCESS'.format(
+        hdfs_path_str="oride_dw_sqoop/oride_data/data_country_conf",
+        pt='{{ds}}'
+    ),
+    bucket_name='opay-datalake',
+    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
+    dag=dag
+)
+
+# 依赖前一天分区
+ods_sqoop_base_weather_per_10min_df_task = UFileSensor(
+    task_id='ods_sqoop_base_weather_per_10min_df_task',
+    filepath='{hdfs_path_str}/dt={pt}/_SUCCESS'.format(
+        hdfs_path_str="oride_dw_sqoop/bi/weather_per_10min",
+        pt='{{ds}}'
+    ),
+    bucket_name='opay-datalake',
+    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
+    dag=dag
+)
+
+##----------------------------------------- 变量 ---------------------------------------## 
+
+db_name="test_db"
+table_name="test_dim_oride_city"
+hdfs_path="ufile://opay-datalake/oride/oride_dw/"+table_name
+
+
+##----------------------------------------- 任务超时监控 ---------------------------------------## 
+
+def fun_task_timeout_monitor(ds,dag,**op_kwargs):
+
+    dag_ids=dag.dag_id
+
+    msg = [
+        {"db": "test_db", "table":"{dag_name}".format(dag_name=dag_ids), "partition": "country_code=NG/dt={pt}".format(pt=ds), "timeout": "800"}
+    ]
+
+    TaskTimeoutMonitor().set_task_monitor(msg)
+
+task_timeout_monitor= PythonOperator(
+    task_id='task_timeout_monitor',
+    python_callable=fun_task_timeout_monitor,
+    provide_context=True,
+    dag=dag
+)
+
+
+##----------------------------------------- 脚本 ---------------------------------------## 
+
+def test_dim_oride_city_sql_task(ds):
+
+    HQL='''
+    set hive.exec.parallel=true;
+    set hive.exec.dynamic.partition.mode=nonstrict;
+
+INSERT overwrite TABLE {db}.{table} partition(country_code,dt)
+
+SELECT city_id,
+       --城市 ID
+
+       city_name,
+       --城市名称
+
+       nvl(cty.name,-1) as country_name,
+       --国家名称
+
+       shape,
+       --形状：1 圆形, 2 多边形
+
+       area,
+       --区域设置数据
+
+       product_id,
+       --开启的服务类型[1,2,99] 1 专车 2 快车 99 招手停
+
+       avoid_highway_type,
+       --可设置避开高速的服务类型[1,2] 1 专车 2 快车
+
+       validate,
+       --本条数据是否有效 0 无效，1 有效
+       
+       weather.weather, 
+       --该城市当天的天气
+
+       opening_time,--开启时间       
+
+       assign_type, --强派的服务类型[1,2,3] 1 专车 2 快车 3 keke车
+
+       nvl(cty.country_code,'nal') as country_code,
+       --二位国家码
+       
+       '{pt}' AS dt
+FROM
+  (SELECT id AS city_id,
+          --城市 ID
+
+          name AS city_name,
+          --城市名称
+
+          country,
+          --国家
+
+          shape,
+          --形状：1 圆形, 2 多边形
+
+          area,
+          --区域设置数据
+
+          serv_type AS product_id,
+          --开启的服务类型[1,2,99] 1 专车 2 快车 99 招手停
+
+          avoid_highway_type,
+          --可设置避开高速的服务类型[1,2] 1 专车 2 快车
+
+          validate, --本条数据是否有效 0 无效，1 有效
+
+          opening_time,--开启时间                
+          assign_type,--强派的服务类型[1,2,3] 1 专车 2 快车 3 keke车
+
+          allow_flagdown_type, --允许招手停的服务类型 
+                                
+          country_id --国家ID 
+
+FROM oride_dw_ods.ods_sqoop_base_data_city_conf_df
+   WHERE dt='{pt}') cit
+LEFT OUTER JOIN
+  (SELECT *
+   FROM oride_dw_ods.ods_sqoop_base_data_country_conf_df 
+   WHERE dt='{pt}') cty ON cit.country_id=cty.id
+left outer join
+(SELECT t.city AS city,
+              t.weather AS weather
+FROM
+  ( SELECT t.city,
+           t.weather,
+           row_number() over(partition BY t.city
+                             ORDER BY t.counts DESC) row_num  --城市一天的天气状况取一天当中某种天气持续最长时间的
+   FROM
+     ( SELECT city,
+              weather,
+              count(1) counts
+      FROM oride_dw_ods.ods_sqoop_base_weather_per_10min_df
+      WHERE dt = '{pt}'
+        AND daliy = '{pt}'
+      GROUP BY city,
+               weather ) t ) t
+WHERE t.row_num = 1) weather
+on lower(cit.city_name)=lower(weather.city)
+
+'''.format(
+        pt=ds,
+        now_day='{{macros.ds_add(ds, +1)}}',
+        now_hour='{{ execution_date.strftime("%H") }}',
+        table=table_name,
+        db=db_name
+        )
+    return HQL
+
+
+#熔断数据，如果数据为0，报错
+def check_key_data_cnt_task(ds):
+
+    cursor = get_hive_cursor()
+
+    #主键重复校验
+    check_sql='''
+    SELECT count(1) as cnt
+      FROM {db}.{table}
+      WHERE dt='{pt}'
+      and country_code in ('NG')
+    '''.format(
+        pt=ds,
+        now_day=airflow.macros.ds_add(ds, +1),
+        table=table_name,
+        db=db_name
+        )
+
+    logging.info('Executing 主键重复校验: %s', check_sql)
+
+    cursor.execute(check_sql)
+
+    res = cursor.fetchone()
+ 
+    if res[0] ==0:
+        flag=1
+        raise Exception ("Error The primary key repeat !", res)
+        sys.exit(1)
+    else:
+        flag=0
+        print("-----> Notice Data Export Success ......")
+
+    return flag
+
+
+
+
+#熔断数据，如果数据重复，报错
+def check_key_data_task(ds):
+
+    cursor = get_hive_cursor()
+
+    #主键重复校验
+    check_sql='''
+    SELECT count(1)-count(distinct city_id) as cnt
+      FROM {db}.{table}
+      WHERE dt='{pt}'
+    '''.format(
+        pt=ds,
+        now_day=airflow.macros.ds_add(ds, +1),
+        table=table_name,
+        db=db_name
+        )
+
+    logging.info('Executing 主键重复校验: %s', check_sql)
+
+    cursor.execute(check_sql)
+
+    res = cursor.fetchone()
+ 
+    if res[0] >1:
+        flag=1
+        raise Exception ("Error The primary key repeat !", res)
+        sys.exit(1)
+    else:
+        flag=0
+        print("-----> Notice Data Export Success ......")
+
+    return flag
+
+
+
+#主流程
+def execution_data_task_id(ds,**kargs):
+
+    hive_hook = HiveCliHook()
+
+    CountriesPublicFrame(ds,db_name,table_name,hdfs_path,"true","true").delete_partition()
+
+
+    #读取sql
+    _sql=test_dim_oride_city_sql_task(ds)
+
+    logging.info('Executing: %s', _sql)
+
+    #执行Hive
+    hive_hook.run_cli(_sql)
+
+    #熔断数据，如果数据不能为0
+    check_key_data_cnt_task(ds)
+
+    #熔断数据
+    check_key_data_task(ds)
+
+    #生成_SUCCESS
+    """
+    第一个参数true: 数据目录是有country_code分区。false 没有
+    第二个参数true: 数据有才生成_SUCCESS false 数据没有也生成_SUCCESS 
+
+    """
+    CountriesPublicFrame(ds,db_name,table_name,hdfs_path,"true","true").touchz_success()
+    
+test_dim_oride_city_task= PythonOperator(
+    task_id='test_dim_oride_city_task',
+    python_callable=execution_data_task_id,
+    provide_context=True,
+    dag=dag
+)
+
+
+ods_sqoop_base_data_city_conf_df_tesk>>test_dim_oride_city_task
+ods_sqoop_base_data_country_conf_df_tesk>>test_dim_oride_city_task
+ods_sqoop_base_weather_per_10min_df_task>>test_dim_oride_city_task
---
+++ plugins/CountriesPublicFrame.py
@@ -0,0 +1,359 @@
+import airflow
+from datetime import datetime, timedelta
+from airflow.operators.hive_operator import HiveOperator
+from utils.connection_helper import get_hive_cursor
+from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.hooks.redis_hook import RedisHook
+from airflow.hooks.hive_hooks import HiveCliHook
+from airflow.operators.hive_to_mysql import HiveToMySqlTransfer
+from airflow.operators.mysql_operator import MySqlOperator
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from airflow.sensors.hive_partition_sensor import HivePartitionSensor
+from airflow.operators.bash_operator import BashOperator
+from airflow.sensors.named_hive_partition_sensor import NamedHivePartitionSensor
+from plugins.comwx import ComwxApi
+import json
+import logging
+from airflow.models import Variable
+import requests
+import os,sys,time
+from plugins.TaskTimeoutMonitor import TaskTimeoutMonitor
+from airflow.utils.trigger_rule import TriggerRule
+
+
+
+class CountriesPublicFrame(object):
+
+    def __init__(self,ds,db_name,table_name,data_hdfs_path,country_partition="true",file_type="true",hour=None):
+
+        self.comwx = ComwxApi('wwd26d45f97ea74ad2', 'BLE_v25zCmnZaFUgum93j3zVBDK-DjtRkLisI_Wns4g', '1000011')
+
+        self.table_name=table_name
+        self.hdfs_data_dir_str=""
+        self.data_hdfs_path=data_hdfs_path
+        self.db_name=db_name
+        self.ds=ds
+        self.country_partition=country_partition
+        self.file_type=file_type
+        self.hour=hour
+
+    def get_country_code(self):
+
+        """
+            获取当前表中所有二位国家码
+        """
+
+        cursor = get_hive_cursor()
+        
+        get_sql='''
+    
+        select concat_ws(',',collect_set(country_code)) as country_code from {db}.{table} WHERE dt='{pt}'
+    
+        '''.format(
+            pt=self.ds,
+            table=self.table_name,
+            db=self.db_name
+            )
+    
+        cursor.execute(get_sql)
+    
+        res = cursor.fetchone()
+
+        if len(res[0]) >1:
+            country_code_list=res[0]
+    
+            logging.info('Executing 二位国家码: %s', country_code_list)
+    
+        else:
+    
+            country_code_list="nal"
+    
+            logging.info('Executing 二位国家码为空，赋予默认值 %s', country_code_list)
+    
+        return country_code_list
+
+
+    def check_success_exist(self):
+
+        """
+            验证_SUCCESS是否执行成功
+        """
+
+        time.sleep(15)
+
+        print("debug-> check_success_exist")
+
+        command="hadoop dfs -ls {hdfs_data_dir}/_SUCCESS>/dev/null 2>/dev/null && echo 1 || echo 0".format(hdfs_data_dir=self.hdfs_data_dir_str)
+
+        logging.info(command)
+
+        out = os.popen(command, 'r')
+        res = out.readlines()
+        
+        res = 0 if res is None else res[0].lower().strip()
+        out.close()
+
+        #判断 _SUCCESS 文件是否生成
+        if res== '' or res == 'None' or res[0] == '0':
+            logging.info("_SUCCESS 验证失败")
+
+            sys.exit(1)
+        
+        else:
+        
+            logging.info("_SUCCESS 验证成功")
+
+
+    def delete_exist_partition(self):
+
+        """
+            删除已有分区，保证数据唯一性
+        """
+
+        time.sleep(10)
+
+        print("debug-> delete_exist_partition")
+
+        #删除语句
+        del_command="hadoop dfs -rm -r {hdfs_data_dir}".format(hdfs_data_dir=self.hdfs_data_dir_str)
+
+        logging.info(del_command)
+
+        os.popen(del_command, 'r')
+
+
+        time.sleep(10)
+
+        #验证删除分区是否存在
+        check_command="hadoop dfs -ls {hdfs_data_dir}>/dev/null 2>/dev/null && echo 1 || echo 0".format(hdfs_data_dir=self.hdfs_data_dir_str)
+
+        out = os.popen(check_command, 'r')
+
+        res = out.readlines()
+        
+        res = 0 if res is None else res[0].lower().strip()
+        out.close()
+
+        print(res)
+
+        #判断 删除分区是否存在
+        if res== '' or res == 'None' or res[0] == '0':
+
+            logging.info("目录删除成功")
+        
+        else:
+
+            #目录存在
+            logging.info("目录删除失败:"+" "+"{hdfs_data_dir}".format(hdfs_data_dir=self.hdfs_data_dir_str))
+
+            sys.exit(1)
+
+        
+   
+    def data_not_file_type_touchz(self):
+
+        """
+            非空文件 touchz _SUCCESS
+        """
+
+        try:
+
+            print("debug-> data_not_file_type_touchz")
+
+            mkdir_str="$HADOOP_HOME/bin/hadoop fs -mkdir -p {hdfs_data_dir}".format(hdfs_data_dir=self.hdfs_data_dir_str)
+
+            logging.info(mkdir_str)
+
+            os.popen(mkdir_str)
+
+            time.sleep(10)
+
+            succ_str="$HADOOP_HOME/bin/hadoop fs -touchz {hdfs_data_dir}/_SUCCESS".format(hdfs_data_dir=self.hdfs_data_dir_str)
+    
+            logging.info(succ_str)
+    
+            os.popen(succ_str)
+    
+            logging.info("DATA EXPORT Successed ......")
+
+            self.check_success_exist()
+
+    
+        except Exception as e:
+
+            #self.comwx.postAppMessage('DW调度系统任务 {jobname} 数据产出异常'.format(jobname=self.table_name),'271')
+
+            logging.info(e)
+
+            sys.exit(1)
+
+
+    def data_file_type_touchz(self):
+
+        """
+            空文件 touchz _SUCCESS
+        """
+
+        try:
+
+            print("debug-> data_file_type_touchz") 
+        
+            #判断数据文件是否为0
+            line_str="$HADOOP_HOME/bin/hadoop fs -du -s {hdfs_data_dir} | tail -1 | awk \'{{print $1}}\'".format(hdfs_data_dir=self.hdfs_data_dir_str)
+    
+            logging.info(line_str)
+        
+            with os.popen(line_str) as p:
+                line_num=p.read()
+        
+            #数据为0，发微信报警通知
+            if line_num[0] == str(0):
+                
+                self.comwx.postAppMessage('DW调度系统任务 {jobname} 数据产出异常'.format(jobname=self.table_name), '271')
+
+                logging.info("Error : {hdfs_data_dir} is empty".format(hdfs_data_dir=self.hdfs_data_dir_str))
+                sys.exit(1)
+        
+            else:  
+
+                time.sleep(5)
+
+                succ_str="hadoop fs -touchz {hdfs_data_dir}/_SUCCESS".format(hdfs_data_dir=self.hdfs_data_dir_str)
+    
+                logging.info(succ_str)
+        
+                os.popen(succ_str)
+        
+                logging.info("DATA EXPORT Successed ......")
+
+            self.check_success_exist()
+
+    
+        except Exception as e:
+
+            #self.comwx.postAppMessage('DW调度系统任务 {jobname} 数据产出异常'.format(jobname=self.table_name),'271')
+
+            logging.info(e)
+
+            sys.exit(1)
+
+
+    def delete_partition(self):
+
+        object_task=self.delete_exist_partition()
+
+        self.countries_data_dir(object_task)
+
+    def touchz_success(self):
+
+        # 没有国家分区并且每个目录必须有数据才能生成 Success
+        if country_partition.lower()=="false" and file_type.lower()=="true":
+
+            self.countries_data_dir(self.data_file_type_touchz())
+
+        # 没有国家分区并且数据为空也生成 Success
+        if country_partition.lower()=="false" and file_type.lower()=="false":
+
+            self.countries_data_dir(self.data_not_file_type_touchz())
+            
+
+        #有国家分区并且每个目录必须有数据才能生成 Success
+        if country_partition.lower()=="true" and file_type.lower()=="true":
+
+            self.countries_data_dir(self.data_file_type_touchz())
+            
+        
+        #有国家分区并且数据为空也生成 Success
+        if country_partition.lower()=="true" and file_type.lower()=="false":
+
+            self.countries_data_dir(self.data_not_file_type_touchz())
+            
+
+
+    def countries_data_dir(self,object_task):
+
+        """
+        country_partition:是否有国家分区
+        file_type:是否空文件也生成 success
+            
+        """
+
+        try:
+
+        
+            # 没有国家分区并且每个目录必须有数据才能生成 Success
+            if country_partition.lower()=="false" and file_type.lower()=="true":
+
+                if hour is None:
+                    #输出不同国家的数据路径
+                    self.hdfs_data_dir_str=data_hdfs_path+"/dt="+self.ds
+                else:
+                    #输出不同国家的数据路径
+                    self.hdfs_data_dir_str=data_hdfs_path+"/dt="+self.ds+"/hour="+hour
+
+                object_task
+
+                return
+
+            # 没有国家分区并且数据为空也生成 Success
+            if country_partition.lower()=="false" and file_type.lower()=="false":
+
+                if hour is None:
+                    #输出不同国家的数据路径
+                    self.hdfs_data_dir_str=data_hdfs_path+"/dt="+self.ds
+                else:
+                    #输出不同国家的数据路径
+                    self.hdfs_data_dir_str=data_hdfs_path+"/dt="+self.ds+"/hour="+hour
+
+                object_task
+
+                return
+
+
+            #获取国家列表
+            country_code_list=self.get_country_code()
+
+
+            for country_code_word in country_code_list.split(","):
+
+
+                #有国家分区并且每个目录必须有数据才能生成 Success
+                if country_partition.lower()=="true" and file_type.lower()=="true":
+
+                    if hour is None:
+
+                        #输出不同国家的数据路径
+                        self.hdfs_data_dir_str=data_hdfs_path+"/country_code="+country_code_word+"/dt="+self.ds
+                    else:
+
+                        #输出不同国家的数据路径
+                        self.hdfs_data_dir_str=data_hdfs_path+"/country_code="+country_code_word+"/dt="+self.ds+"/hour="+hour
+
+
+                    object_task
+
+                
+                #有国家分区并且数据为空也生成 Success
+                if country_partition.lower()=="true" and file_type.lower()=="false":
+
+
+                    if hour is None:
+
+                        #输出不同国家的数据路径
+                        self.hdfs_data_dir_str=data_hdfs_path+"/country_code="+country_code_word+"/dt="+self.ds
+                    else:
+
+                        #输出不同国家的数据路径
+                        self.hdfs_data_dir_str=data_hdfs_path+"/country_code="+country_code_word+"/dt="+self.ds+"/hour="+hour
+
+                    object_task
+            
+        except Exception as e:
+
+            #self.comwx.postAppMessage('DW调度系统任务 {jobname} 数据产出异常'.format(jobname=table_name),'271')
+
+            logging.info(e)
+
+            sys.exit(1)
+
+            
