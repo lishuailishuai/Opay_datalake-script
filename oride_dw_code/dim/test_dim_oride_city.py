16d0daa9bc0f2f064921941ffb0af454f73c230c
杨明泽 <mingze.yang@opay-inc.com>
2019-11-26 21:33:56 +0800

    no message

--- oride_dw_code/dim/test_dim_oride_city.py
+++ oride_dw_code/dim/test_dim_oride_city.py
@@ -179,7 +179,7 @@ FROM
               weather,
               count(1) counts
       FROM oride_dw_ods.ods_sqoop_base_weather_per_10min_df
-      WHERE dt = '{pt}'
+      WHERE dt = '{now_hour}'
         AND daliy = '{now_day}'
       GROUP BY city,
                weather ) t ) t
@@ -189,7 +189,7 @@ on lower(cit.city_name)=lower(weather.city)
 '''.format(
         pt=ds,
         #now_day='{{macros.ds_add(ds, +1)}}',
-        now_hour='{{ execution_date.strftime("%H") }}',
+        now_hour=execution_date.strftime("%H"),
         now_day=airflow.macros.ds_add(ds, +1),
         table=table_name,
         db=db_name
