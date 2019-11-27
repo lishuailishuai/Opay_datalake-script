f332948ed59fcdc750e4ea30e4689de13c1c9a10
杨明泽 <mingze.yang@opay-inc.com>
2019-11-27 19:01:40 +0800

    no message

--- opay_owealth_dw/opay_owealth_source_sqoop_hf.py
+++ opay_owealth_dw/opay_owealth_source_sqoop_hf.py
@@ -188,7 +188,7 @@ for db_name, table_name, conn_id, prefix_name,priority_weight_nm in table_list:
             --username {username} \
             --password {password} \
             --table {table} \
-            --target-dir {ufile_path}/dt={{{{airflow.macros.ds_add({{{{ ds }}}}, +1)}}}}/hour={{{{ execution_date.strftime("%H") }}}} \
+            --target-dir {ufile_path}/dt={{{{airflow.macros.ds_add(ds, +1)}}}}/hour={{{{ execution_date.strftime("%H") }}}} \
             --fields-terminated-by "\\001" \
             --lines-terminated-by "\\n" \
             --hive-delims-replacement " " \
