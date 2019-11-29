7ce2c7dbd160c904510d18fffbc1df5ea0fed491
杨明泽 <mingze.yang@opay-inc.com>
2019-11-29 10:08:04 +0800

    no message

--- opos_dw/opos_source_sqoop_df.py
+++ opos_dw/opos_source_sqoop_df.py
@@ -217,6 +217,7 @@ for db_name, table_name, conn_id, prefix_name in table_list:
         op_kwargs={
             'db_name': HIVE_DB,
             'table_name': hive_table_name,
+            'is_valid_success':"true"
         },
         dag=dag
     )
