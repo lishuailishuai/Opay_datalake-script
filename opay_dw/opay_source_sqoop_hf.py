cde5074fa2ff47e7b9f95154152784359875d4a6
杨明泽 <mingze.yang@opay-inc.com>
2019-11-29 10:24:59 +0800

    no message

--- opay_dw/opay_source_sqoop_hf.py
+++ opay_dw/opay_source_sqoop_hf.py
@@ -250,19 +250,19 @@ for db_name, table_name, conn_id, prefix_name,priority_weight_nm in table_list:
 
     if table_name in IGNORED_TABLE_LIST:
         add_partitions >> validate_all_data
-    else:
-        # 数据量监控
-        volume_monitoring = PythonOperator(
-            task_id='volume_monitorin_{}'.format(hive_table_name),
-            python_callable=data_volume_monitoring,
-            provide_context=True,
-            op_kwargs={
-                'db_name': HIVE_DB,
-                'table_name': hive_table_name,
-            },
-            dag=dag
-        )
-        add_partitions >> volume_monitoring >> validate_all_data
+    # else:
+    #     # 数据量监控
+    #     volume_monitoring = PythonOperator(
+    #         task_id='volume_monitorin_{}'.format(hive_table_name),
+    #         python_callable=data_volume_monitoring,
+    #         provide_context=True,
+    #         op_kwargs={
+    #             'db_name': HIVE_DB,
+    #             'table_name': hive_table_name,
+    #         },
+    #         dag=dag
+    #     )
+    #     add_partitions >> volume_monitoring >> validate_all_data
 
     # 超时监控
     task_timeout_monitor= PythonOperator(
