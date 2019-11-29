0fb27d61c04e60d46b50b1a14340f3bdc8258d4b
杨明泽 <mingze.yang@opay-inc.com>
2019-11-29 10:06:35 +0800

    no message

--- oride_dw/oride_source_sqoop_di.py
+++ oride_dw/oride_source_sqoop_di.py
@@ -249,6 +249,7 @@ for db_name, table_name, conn_id, prefix_name,priority_weight_nm in table_list:
         op_kwargs={
             'db_name': HIVE_DB,
             'table_name': hive_table_name,
+            'is_valid_success':"true"
         },
         dag=dag
     )
