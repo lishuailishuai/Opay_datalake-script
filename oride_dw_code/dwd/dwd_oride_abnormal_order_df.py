93ea5d9e745c75f9dab24fb29d2dff3f2ce396a0
杨明泽 <mingze.yang@opay-inc.com>
2019-11-30 19:39:33 +0800

    no message

--- oride_dw_code/dwd/dwd_oride_abnormal_order_df.py
+++ oride_dw_code/dwd/dwd_oride_abnormal_order_df.py
@@ -26,7 +26,7 @@ import os
 
 args = {
         'owner': 'yangmingze',
-        'start_date': datetime(2019, 11, 9),
+        'start_date': datetime(2019, 11, 28),
         'depends_on_past': False,
         'retries': 3,
         'retry_delay': timedelta(minutes=2),
