440e5862e3f0b1d013391a7689a00fbf9d6393dc
杨明泽 <mingze.yang@opay-inc.com>
2019-11-30 19:47:04 +0800

    no message

--- oride_dw_code/dwd/dwd_oride_abnormal_order_df.py
+++ oride_dw_code/dwd/dwd_oride_abnormal_order_df.py
@@ -186,7 +186,7 @@ def execution_data_task_id(ds,**kwargs):
 
     """
 
-    cf=CountriesPublicFrame(ds,db_name,table_name,hdfs_path,"true","true")
+    cf=CountriesPublicFrame("true",ds,db_name,table_name,hdfs_path,"true","true")
 
     #删除分区
     cf.delete_partition()
