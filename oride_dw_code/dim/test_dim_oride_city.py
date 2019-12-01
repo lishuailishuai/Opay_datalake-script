f7b808867f961e7f99ba6be186dfa2e1fac26127
杨明泽 <mingze.yang@opay-inc.com>
2019-12-01 20:17:16 +0800

    no message

--- oride_dw_code/dim/test_dim_oride_city.py
+++ oride_dw_code/dim/test_dim_oride_city.py
@@ -294,7 +294,7 @@ def execution_data_task_id(ds,**kwargs):
 
     """
 
-    cf=CountriesPublicFrame("true",ds,db_name,table_name,hdfs_path,"true","true")
+    cf=CountriesPublicFrame("true",ds,db_name,table_name,hdfs_path,"true","false")
 
     #删除分区
     cf.delete_partition_dev()
