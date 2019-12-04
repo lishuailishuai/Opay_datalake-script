b08bd872442c630518c533fb8d9e6573274368e7
feng.yuan <feng.yuan@opay-inc.com>
2019-12-04 10:00:15 +0800

    Default Changelist

--- oride_dw_code/dwd/dwd_oride_order_base_include_test_di.py
+++ oride_dw_code/dwd/dwd_oride_order_base_include_test_di.py
@@ -726,7 +726,7 @@ def execution_data_task_id(ds,**kwargs):
     """
 
 
-    cf=CountriesPublicFrame("true",ds,db_name,table_name,hdfs_path,"true","false")
+    cf=CountriesPublicFrame("true",ds,db_name,table_name,hdfs_path,"true","true")
 
     #删除分区
     cf.delete_partition()
