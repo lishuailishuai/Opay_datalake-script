a8f45f25bbfad7ff489d5d0d50dc4699478367e5
shuzhen.liu <shuzhen.liu@opay-inc.com>
2019-12-04 21:16:17 +0800

    x

--- opay_dw_code/app/app_opay_owealth_air_mobile_d.py
+++ opay_dw_code/app/app_opay_owealth_air_mobile_d.py
@@ -128,7 +128,7 @@ def execution_data_task_id(ds, **kargs):
     第二个参数true: 数据有才生成_SUCCESS false 数据没有也生成_SUCCESS 
 
     """
-    TaskTouchzSuccess().countries_touchz_success(ds, db_name, table_name, hdfs_path, "false", "true")
+    TaskTouchzSuccess().countries_touchz_success(airflow.macros.ds_add(ds, +1), db_name, table_name, hdfs_path, "false", "true")
 
 
 app_opay_owealth_air_mobile_d_task = PythonOperator(
