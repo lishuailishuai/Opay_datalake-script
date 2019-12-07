63cb3879527592fb9c48b1779f379ed68f0a88f0
杨明泽 <mingze.yang@opay-inc.com>
2019-12-07 10:30:26 +0800

    no message

--- oride_dw_code/dwd/dwd_oride_order_base_include_test_di.py
+++ oride_dw_code/dwd/dwd_oride_order_base_include_test_di.py
@@ -740,7 +740,7 @@ def execution_data_task_id(ds,**kwargs):
     cf=CountriesPublicFrame("true",ds,db_name,table_name,hdfs_path,"true","true")
 
     #删除分区
-    cf.delete_partition()
+    #cf.delete_partition()
 
     #读取sql
     _sql="\n"+cf.alter_partition()+"\n"+dwd_oride_order_base_include_test_di_sql_task(ds)
