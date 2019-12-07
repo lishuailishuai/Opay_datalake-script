24e2345478081b7c7f9d6e55f1c80fde7332281d
杨明泽 <mingze.yang@opay-inc.com>
2019-12-07 10:35:07 +0800

    no message

--- oride_dw_code/dwd/dwd_oride_finance_driver_repayment_extend_df.py
+++ oride_dw_code/dwd/dwd_oride_finance_driver_repayment_extend_df.py
@@ -16,7 +16,7 @@ from airflow.sensors.named_hive_partition_sensor import NamedHivePartitionSensor
 from airflow.sensors.hive_partition_sensor import HivePartitionSensor
 from airflow.sensors import UFileSensor
 from plugins.TaskTimeoutMonitor import TaskTimeoutMonitor
-from plugins.TaskTouchzSuccess import TaskTouchzSuccess
+from plugins.CountriesPublicFrame import CountriesPublicFrame
 import json
 import logging
 from airflow.models import Variable
