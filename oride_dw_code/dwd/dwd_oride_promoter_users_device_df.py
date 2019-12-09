eb0ce57c09f8989f6edd2bcc7586f53fc6d7e2cb
杨明泽 <mingze.yang@opay-inc.com>
2019-12-09 19:17:37 +0800

    no message

--- oride_dw_code/dwd/dwd_oride_promoter_users_device_df.py
+++ oride_dw_code/dwd/dwd_oride_promoter_users_device_df.py
@@ -36,7 +36,7 @@ args = {
 } 
 
 dag = airflow.DAG( 'dwd_oride_promoter_users_device_df', 
-    schedule_interval="00 01 * * *", 
+    schedule_interval="00 03 * * *", 
     default_args=args,
     catchup=False) 
 
