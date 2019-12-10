e2c41043e33242f59c287a2bbd02e028eb7c8140
xiedong <dong.xie@opay-inc.com>
2019-12-10 15:22:49 +0800

    按国家统计

--- opay_dw_code/dm/dm_opay_transaction_originator_base_cube_d.py
+++ opay_dw_code/dm/dm_opay_transaction_originator_base_cube_d.py
@@ -169,8 +169,7 @@ def dm_opay_transaction_originator_base_cube_d_sql_task(ds):
         (country_code, top_service_type, sub_service_type, originator_type),
         (country_code, top_service_type, sub_service_type),
         (country_code, top_service_type),
-        (country_code),
-        ()
+        (country_code)
     )
 
     '''.format(
