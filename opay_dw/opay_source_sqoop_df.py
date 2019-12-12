22dae6212636df535974109336701887e925b746
zhangzhenqian <jasonzhang081114@gmail.com>
2019-12-12 16:33:29 +0800

    opay迁移oss
    Test Plan: none
    
    Reviewers: nan.li, dong.xie
    
    Reviewed By: dong.xie
    
    Maniphest Tasks: T5360
    
    Differential Revision: https://pha.static.kunlun.com/D16927

--- opay_dw/opay_source_sqoop_df.py
+++ opay_dw/opay_source_sqoop_df.py
@@ -154,7 +154,7 @@ table_list = [
 
 HIVE_DB = 'opay_dw_ods'
 HIVE_TABLE = 'ods_sqoop_%s_%s_df'
-UFILE_PATH = Variable.get("OBJECT_STORAGE_PROTOCOL") + 'opay-datalake/opay_dw_ods/%s/%s'
+UFILE_PATH = 'oss://opay-datalake/opay_dw_sqoop/%s/%s'
 ODS_CREATE_TABLE_SQL = '''
     CREATE EXTERNAL TABLE IF NOT EXISTS {db_name}.`{table_name}`(
         {columns}
