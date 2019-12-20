d8747b5e6d6ca4b07062bfa964d941567a4115ac
xiedong <dong.xie@opay-inc.com>
2019-12-20 21:24:43 +0800

    修改

--- opay_dw_code/ods/ods_opay_service_scenario_mapping_df.py
+++ opay_dw_code/ods/ods_opay_service_scenario_mapping_df.py
@@ -95,7 +95,7 @@ def execution_data_task_id(ds, **kargs):
     第二个参数true: 数据有才生成_SUCCESS false 数据没有也生成_SUCCESS 
 
     """
-    TaskTouchzSuccess().countries_touchz_success(ds, db_name, table_name, hdfs_path, "true", "true")
+    TaskTouchzSuccess().countries_touchz_success(ds, db_name, table_name, hdfs_path, "true", "false")
 
 ods_opay_service_scenario_mapping_df_task = PythonOperator(
     task_id='ods_opay_service_scenario_mapping_df_task',
