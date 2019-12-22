b73337e258e7156665c202c159203cd3fd8a91d3
chenghui <hui.cheng@opay-inc.com>
2019-12-22 14:38:13 +0800

    修改依赖
    Test Plan: yy
    
    Reviewers: shuzhen.liu
    
    Reviewed By: shuzhen.liu
    
    Differential Revision: https://pha.static.kunlun.com/D18227

--- oride_dw_code/dwd/dwd_oride_anti_fraud_log_di.py
+++ oride_dw_code/dwd/dwd_oride_anti_fraud_log_di.py
@@ -43,9 +43,9 @@ dag = airflow.DAG( 'dwd_oride_anti_fraud_log_di',
 
 
 #依赖前一天分区
-log_anti_ofood_oride_fraud_task=HivePartitionSensor(
-      task_id="log_anti_ofood_oride_fraud_task",
-      table="log_anti_ofood_oride_fraud",
+log_anti_oride_fraud_task=HivePartitionSensor(
+      task_id="log_anti_oride_fraud_task",
+      table="log_anti_oride_fraud",
       partition="dt='{{macros.ds_add(ds, +1)}}' and hour='00'",
       schema="oride_source",
       poke_interval=60, #依赖不满足时，一分钟检查一次依赖状态
@@ -87,45 +87,23 @@ def dwd_oride_anti_fraud_log_di_sql_task(ds):
     set hive.exec.parallel=true;
     set hive.exec.dynamic.partition.mode=nonstrict;
 
-INSERT overwrite TABLE oride_dw.{table} partition(country_code,dt)
-select 
-action,--行为
-userid,--用户id
-deviceid,--设备id
-inviterrole,--邀请人类型
-inviterid,--邀请人id
-isblacklisted,--是否在黑名单
-isvirtualdevice,--是否是虚拟设备
-driverid,--司机id
-isroot,--是否root
-isvirtual,--是否为虚拟设备
-taketime,--接单时间
-waittime,--到达接送点时间
-pickuptime,--接到乘客时间
-arrivetime,--到达终点时间
-canceltime,--订单取消时间
-cancelreason,--订单取消原因
-createtime,--创建时间
-silencefrom,--静默开始时间
-silenceto,--静默结束时间
-behaviors,--行为id
-behavior,--行为id
-silenttime,--静默时间
-waitlat,--到达接送点纬度
-waitlng,--到达接送点经度
-arrivelat,--到达终点纬度
-arrivelng,--到达终点经度
-distance1,--司机等待乘客的位置与出发地
-distance2,--到达地和目的地的距离
-userdeviceid,--用户设备id
-issilent,--是否静默
-reason,--原因
-couponid,--优惠类型
-orderid,--订单id
-abnormalstrategy, --命中策略id
-'nal' as country_code,
-dt
- from  oride_source.log_anti_ofood_oride_fraud where dt='{pt}' and action in ('UserRegister','UserLogin','DriverLogin','SilenceUser','UserCancelOrder','DriverCancelOrder','OrderWait','SilenceDriver','OrderArrive','SilenceDriver2','SilenceDriver3','OrderFinish','IsUserSilent','IsDriverSilent')
+    INSERT overwrite TABLE oride_dw.{table} partition(country_code,dt)
+    
+    select action,--行为
+        behavior_id,--行为id
+        user_id,--用户id
+        driver_id,--司机id
+        order_id,--订单id
+        extend,--扩展信息json
+        `timestamp`,--时间戳
+        'nal' as country_code,
+        dt
+    from  oride_source.log_anti_oride_fraud 
+    where dt='{pt}' 
+    and action in ('UserRegister','UserLogin','DriverLogin',
+        'SilenceUser','UserCancelOrder','DriverCancelOrder','OrderWait','SilenceDriver',
+        'OrderArrive','SilenceDriver2','SilenceDriver3','OrderFinish','IsUserSilent','IsDriverSilent')
+    ;
 
 '''.format(
         pt=ds,
@@ -166,4 +144,4 @@ dwd_oride_anti_fraud_log_di_task= PythonOperator(
     dag=dag
 )
  
-log_anti_ofood_oride_fraud_task>>dwd_oride_anti_fraud_log_di_task
+log_anti_oride_fraud_task>>dwd_oride_anti_fraud_log_di_task
