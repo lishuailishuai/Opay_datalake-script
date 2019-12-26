46703cb21def9842147760a45b0b04b24ce1505f
needjuju <39019332@qq.com>
2019-12-26 17:14:54 +0800

    ufile - oss
    Summary: y
    
    Test Plan: y
    
    Reviewers: hui.cheng
    
    Reviewed By: hui.cheng
    
    Differential Revision: https://pha.static.kunlun.com/D18679

--- oride_dw_code/dwd/dwd_oride_abnormal_order_df.py
+++ oride_dw_code/dwd/dwd_oride_abnormal_order_df.py
@@ -24,6 +24,8 @@ import logging
 from airflow.models import Variable
 import requests
 import os
+from airflow.sensors import OssSensor
+
 
 args = {
         'owner': 'yangmingze',
@@ -41,27 +43,46 @@ dag = airflow.DAG( 'dwd_oride_abnormal_order_df',
     default_args=args,
     catchup=False) 
 
-
-##----------------------------------------- 依赖 ---------------------------------------## 
-
-
-ods_sqoop_base_data_abnormal_order_df_tesk = UFileSensor(
-    task_id='ods_sqoop_base_data_abnormal_order_df_tesk',
-    filepath="{hdfs_path_str}/dt={pt}/_SUCCESS".format(
-        hdfs_path_str="oride_dw_sqoop/oride_data/data_abnormal_order",
-        pt="{{ds}}"
-    ),
-    bucket_name='opay-datalake',
-    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
-    dag=dag
-)
-
-
-##----------------------------------------- 变量 ---------------------------------------## 
+##----------------------------------------- 变量 ---------------------------------------##
 
 db_name="oride_dw"
 table_name="dwd_oride_abnormal_order_df"
-hdfs_path="ufile://opay-datalake/oride/oride_dw/dwd_oride_abnormal_order_df"
+##----------------------------------------- 依赖 ---------------------------------------## 
+
+#获取变量
+code_map=eval(Variable.get("sys_flag"))
+
+#判断ufile(cdh环境)
+if code_map["id"].lower()=="ufile":
+
+    ods_sqoop_base_data_abnormal_order_df_tesk = UFileSensor(
+        task_id='ods_sqoop_base_data_abnormal_order_df_tesk',
+        filepath="{hdfs_path_str}/dt={pt}/_SUCCESS".format(
+            hdfs_path_str="oride_dw_sqoop/oride_data/data_abnormal_order",
+            pt="{{ds}}"
+        ),
+        bucket_name='opay-datalake',
+        poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
+        dag=dag
+    )
+    # 路径
+    hdfs_path = "ufile://opay-datalake/oride/oride_dw/" + table_name
+else:
+    print("成功")
+
+    ods_sqoop_base_data_abnormal_order_df_tesk = OssSensor(
+        task_id='ods_sqoop_base_data_abnormal_order_df_tesk',
+        filepath="{hdfs_path_str}/dt={pt}/_SUCCESS".format(
+            hdfs_path_str="oride_dw_sqoop/oride_data/data_abnormal_order",
+            pt="{{ds}}"
+        ),
+        bucket_name='opay-datalake',
+        poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
+        dag=dag
+    )
+    # 路径
+    hdfs_path = "oss://opay-datalake/oride/oride_dw/" + table_name
+
 
 
 ##----------------------------------------- 任务超时监控 ---------------------------------------## 
