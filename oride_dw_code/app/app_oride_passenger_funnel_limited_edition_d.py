0a1ec1f82d848faa9ab34ec05d5c4c3e971818cf
chenghui <hui.cheng@opay-inc.com>
2019-12-28 15:25:20 +0800

    修改依赖----
    Test Plan: y
    
    Reviewers: jialong.li
    
    Reviewed By: jialong.li
    
    Differential Revision: https://pha.static.kunlun.com/D18922

--- oride_dw_code/app/app_oride_passenger_funnel_limited_edition_d.py
+++ oride_dw_code/app/app_oride_passenger_funnel_limited_edition_d.py
@@ -50,11 +50,14 @@ code_map=eval(Variable.get("sys_flag"))
 
 #判断ufile(cdh环境)
 if code_map["id"].lower()=="ufile":
-    dwd_oride_client_event_detail_hi_task = HivePartitionSensor(
+
+    dwd_oride_client_event_detail_hi_task = UFileSensor(
         task_id="dwd_oride_client_event_detail_hi_task",
-        table="dwd_oride_client_event_detail_hi",
-        partition="dt='{{ds}}'",
-        schema="oride_dw",
+        filepath='{hdfs_path_str}/country_code=nal/dt={pt}/hour=23/_SUCCESS'.format(
+            hdfs_path_str="oride/oride_dw/dwd_oride_client_event_detail_hi",
+            pt='{{ds}}'
+        ),
+        bucket_name='opay-datalake',
         poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
         dag=dag
     )
@@ -64,7 +67,7 @@ else:
     print("成功")
     dwd_oride_client_event_detail_hi_task = OssSensor(
         task_id="dwd_oride_client_event_detail_hi_task",
-        bucket_key='{hdfs_path_str}/dt={pt}/hour=23/_SUCCESS'.format(
+        bucket_key='{hdfs_path_str}/country_code=nal/dt={pt}/hour=23/_SUCCESS'.format(
             hdfs_path_str="oride/oride_dw/dwd_oride_client_event_detail_hi",
             pt='{{ds}}'
         ),
