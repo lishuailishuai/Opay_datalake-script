6c1e1ab3c7ef26c6ca7864a1325c9430439a4961
shuai01.li <shuai01.li@opay-inc.com>
2019-12-31 19:31:00 +0800

    修改

--- opay_dw_code/app/app_opay_device_d.py
+++ opay_dw_code/app/app_opay_device_d.py
@@ -46,7 +46,7 @@ dag = airflow.DAG('app_opay_device_d',
 dwd_opay_client_event_base_di_task = OssSensor(
     task_id='dwd_opay_client_event_base_di_task',
     bucket_key='{hdfs_path_str}/dt={pt}/_SUCCESS'.format(
-        hdfs_path_str="opay-datalake/opay/opay_dw/dwd_opay_client_event_base_di/country_code=nal",
+        hdfs_path_str="opay/opay_dw/dwd_opay_client_event_base_di/country_code=nal",
         pt='{{ds}}'
     ),
     bucket_name='opay-datalake',
