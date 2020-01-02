c665b00c205febd4c75ba1df2e9943122ed2b1da
shuzhen.liu <shuzhen.liu@opay-inc.com>
2020-01-02 20:21:30 +0800

    xiug

--- opay_dw_code/app/app_opay_pos_report_d.py
+++ opay_dw_code/app/app_opay_pos_report_d.py
@@ -27,7 +27,7 @@ import os
 
 args = {
     'owner': 'liushuzhen',
-    'start_date': datetime(2020, 1, 2),
+    'start_date': datetime(2020, 1, 1),
     'depends_on_past': False,
     'retries': 3,
     'retry_delay': timedelta(minutes=2),
