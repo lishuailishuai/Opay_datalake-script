144af2f338028e8b0885b2fd41f9da50e62a42d2
chenghui <hui.cheng@opay-inc.com>
2020-01-02 11:38:23 +0800

    添加dev
    Test Plan: y
    
    Reviewers: jialong.li
    
    Reviewed By: jialong.li
    
    Differential Revision: https://pha.static.kunlun.com/D19272

--- oride_dw_code/dim/dim_oride_driver_base_dev.py
+++ oride_dw_code/dim/dim_oride_driver_base_dev.py
@@ -27,7 +27,7 @@ import os
 
 args = {
         'owner': 'chenghui',
-        'start_date': datetime(2020, 1, 02),
+        'start_date': datetime(2020, 1, 1),
         'depends_on_past': False,
         'retries': 3,
         'retry_delay': timedelta(minutes=2),
