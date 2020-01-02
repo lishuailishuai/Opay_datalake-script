f90499565f2f6b5356de17ed8d99888d48cf1960
chenlili <lili.chen@opay-inc.com>
2020-01-02 20:03:41 +0800

    新增dwm层乘客主题表
    Test Plan: no
    
    Reviewers: jialong.li
    
    Reviewed By: jialong.li
    
    Differential Revision: https://pha.static.kunlun.com/D19355

--- oride_dw_code/dwd/dwd_oride_order_base_di.py
+++ oride_dw_code/dwd/dwd_oride_order_base_di.py
@@ -46,16 +46,11 @@ dag = airflow.DAG('dwd_oride_order_base_di',
 
 db_name = "oride_dw"
 table_name = "dwd_oride_order_base_di"
+hdfs_path = "ufile://opay-datalake/oride/oride_dw/" + table_name
 
 ##----------------------------------------- 依赖 ---------------------------------------##
-#获取变量
-code_map=eval(Variable.get("sys_flag"))
-
-#判断ufile(cdh环境)
-if code_map["id"].lower()=="ufile":
-
-    # 依赖前一天分区
-    ods_binlog_data_order_hi_prev_day_task = WebHdfsSensor(
+# 依赖前一天分区
+ods_binlog_data_order_hi_prev_day_task = WebHdfsSensor(
         task_id='ods_binlog_data_order_hi_prev_day_task',
         filepath='{hdfs_path_str}/dt={now_day}/hour=00/_SUCCESS'.format(
             hdfs_path_str="/user/hive/warehouse/oride_dw_ods.db/ods_binlog_data_order_hi",
@@ -66,8 +61,8 @@ if code_map["id"].lower()=="ufile":
         dag=dag
     )
 
-    # 依赖前一天分区
-    ods_sqoop_base_data_order_payment_df_prev_day_task = UFileSensor(
+# 依赖前一天分区
+ods_sqoop_base_data_order_payment_df_prev_day_task = UFileSensor(
         task_id='ods_sqoop_base_data_order_payment_df_prev_day_task',
         filepath='{hdfs_path_str}/dt={pt}/_SUCCESS'.format(
             hdfs_path_str="oride_dw_sqoop/oride_data/data_order_payment",
@@ -78,9 +73,8 @@ if code_map["id"].lower()=="ufile":
         dag=dag
     )
 
-    # 依赖前一天分区
-    # 依赖前一天分区
-    oride_client_event_detail_prev_day_task = UFileSensor(
+# 依赖前一天分区
+oride_client_event_detail_prev_day_task = UFileSensor(
         task_id="oride_client_event_detail_prev_day_task",
         filepath='{hdfs_path_str}/country_code=nal/dt={pt}/hour=23/_SUCCESS'.format(
             hdfs_path_str="oride/oride_dw/dwd_oride_client_event_detail_hi",
@@ -91,8 +85,8 @@ if code_map["id"].lower()=="ufile":
         dag=dag
     )
 
-    # 依赖前一天分区
-    ods_sqoop_base_data_country_conf_df_prev_day_task = UFileSensor(
+# 依赖前一天分区
+ods_sqoop_base_data_country_conf_df_prev_day_task = UFileSensor(
         task_id='ods_sqoop_base_data_country_conf_df_prev_day_task',
         filepath='{hdfs_path_str}/dt={pt}/_SUCCESS'.format(
             hdfs_path_str="oride_dw_sqoop/oride_data/data_country_conf",
@@ -102,59 +96,7 @@ if code_map["id"].lower()=="ufile":
         poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
         dag=dag
     )
-    hdfs_path = "ufile://opay-datalake/oride/oride_dw/" + table_name
 
-else:
-    print("成功")
-    # 依赖前一天分区
-    ods_binlog_data_order_hi_prev_day_task = OssSensor(
-        task_id='ods_binlog_base_data_order_hi_prev_day_task',
-        bucket_key='{hdfs_path_str}/dt={now_day}/hour=00/_SUCCESS'.format(
-            hdfs_path_str="oride_binlog/oride_db.oride_data.data_order",
-            pt='{{ds}}',
-            now_day='{{macros.ds_add(ds, +1)}}'
-        ),
-        bucket_name='opay-datalake',
-        poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
-        dag=dag
-    )
-
-    # 依赖前一天分区
-    ods_sqoop_base_data_order_payment_df_prev_day_task = OssSensor(
-        task_id='ods_sqoop_base_data_order_payment_df_prev_day_task',
-        bucket_key='{hdfs_path_str}/dt={pt}/_SUCCESS'.format(
-            hdfs_path_str="oride_dw_sqoop/oride_data/data_order_payment",
-            pt='{{ds}}'
-        ),
-        bucket_name='opay-datalake',
-        poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
-        dag=dag
-    )
-
-    # 依赖前一天分区
-    oride_client_event_detail_prev_day_task = OssSensor(
-        task_id="oride_client_event_detail_prev_day_task",
-        bucket_key='{hdfs_path_str}/dt={pt}/hour=23/_SUCCESS'.format(
-            hdfs_path_str="oride/oride_dw/dwd_oride_client_event_detail_hi/country_code=nal",
-            pt='{{ds}}'
-        ),
-        bucket_name='opay-datalake',
-        poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
-        dag=dag
-    )
-
-    # 依赖前一天分区
-    ods_sqoop_base_data_country_conf_df_prev_day_task = OssSensor(
-        task_id='ods_sqoop_base_data_country_conf_df_prev_day_task',
-        bucket_key='{hdfs_path_str}/dt={pt}/_SUCCESS'.format(
-            hdfs_path_str="oride_dw_sqoop/oride_data/data_country_conf",
-            pt='{{ds}}'
-        ),
-        bucket_name='opay-datalake',
-        poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
-        dag=dag
-    )
-    hdfs_path = "oss://opay-datalake/oride/oride_dw/" + table_name
 
 ##----------------------------------------- 任务超时监控 ---------------------------------------##
 
