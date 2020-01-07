6959ae349abd2c253f2700e1f58efe78afaccda1
feng.yuan <feng.yuan@opay-inc.com>
2020-01-07 16:31:31 +0800

    Default Changelist

--- opos_dw_code/app/app_opos_metrics_report_mid.py
+++ opos_dw_code/app/app_opos_metrics_report_mid.py
@@ -102,6 +102,119 @@ set hive.exec.parallel=true;
 set hive.exec.dynamic.partition.mode=nonstrict;
 set hive.strict.checks.cartesian.product=false;
 
+--02.opos_temp.opos_metrcis_report
+drop TABLE opos_dw.app_opos_metrics_report_mid;
+CREATE EXTERNAL TABLE opos_dw.app_opos_metrics_report_mid(
+hcm_id int COMMENT 'HCMid'
+,cm_id int COMMENT 'CMid'
+,rm_id int COMMENT 'RMid'
+,bdm_id int COMMENT 'BDMid'
+,bd_id int COMMENT 'BDid'
+
+,city_id string COMMENT '城市编码'
+
+,merchant_cnt int COMMENT '总商户数'
+,pos_merchant_cnt int COMMENT 'pos商户数'
+,new_merchant_cnt int COMMENT '新增商户数'
+,new_pos_merchant_cnt int COMMENT '新增pos商户数'
+
+,pos_complete_order_cnt bigint COMMENT 'pos完单数'
+,qr_complete_order_cnt bigint COMMENT '二维码完单数'
+,complete_order_cnt bigint COMMENT '完单数'
+,gmv decimal(22,2) COMMENT 'gmv'
+,actual_amount decimal(22,2) COMMENT '实收金额'
+,return_amount double COMMENT '返现金额'
+,new_user_cost double COMMENT '新客获客成本'
+,old_user_cost double COMMENT '老客营销成本'
+,return_amount_order_cnt bigint COMMENT '返现订单数'
+
+--返现主题cashback
+,cashback_order_cnt int COMMENT '返现成功订单数'
+,cashback_fail_order_cnt int COMMENT '返现失败订单数'
+,cashback_order_gmv decimal(20,2) COMMENT '返现订单GMV'
+,cashback_per_order_amt decimal(20,2) COMMENT '返现单均价'
+,cashback_per_people_amt decimal(20,2) COMMENT '返现客单价'
+,cashback_people_cnt int COMMENT '返现客户数量'
+,cashback_first_people_cnt int COMMENT '首单用户数（返现）'
+,cashback_zero_order_cnt int COMMENT '无返现订单数'
+,cashback_order_percent decimal(20,4) COMMENT '返现订单占比'
+,cashback_amt decimal(20,2) COMMENT '返现金额'
+
+--立减主题
+,reduce_order_cnt int COMMENT '立减成功订单数'
+,reduce_zero_order_cnt int COMMENT '无立减订单数量'
+,reduce_amt decimal(20,2) COMMENT '立减金额'
+,reduce_order_gmv decimal(20,2) COMMENT '立减订单GMV'
+,reduce_per_order_amt decimal(20,2) COMMENT '立减单均价'
+,reduce_per_people_amt decimal(20,2) COMMENT '立减客单价'
+,reduce_people_cnt int COMMENT '参与立减用户数'
+,reduce_first_people_cnt int COMMENT '首单用户数（立减）'
+
+--本月新增商户数统计
+,month_newshop_cnt int COMMENT '本月新增商户数'
+
+--优惠券主题
+,coupon_order_cnt int COMMENT '优惠券订单数量'
+,coupon_order_people int COMMENT '用户数（优惠券）'
+,coupon_first_order_people int COMMENT '首单用户数（优惠券）'
+,coupon_pay_amount decimal(20,2) COMMENT '优惠券订单实付金额'
+,coupon_order_gmv decimal(20,2) COMMENT '优惠券订单GMV'
+,coupon_discount_amount decimal(20,2) COMMENT '优惠券核销金额'
+
+,coupon_useless_order_cnt int COMMENT '未使用优惠订单数'
+,coupon_useless_order_people int COMMENT '未使用优惠用户数'
+,coupon_useless_pay_amount decimal(20,2)  COMMENT '未使用优惠券订单实付金额'
+,coupon_useless_order_gmv decimal(20,2) COMMENT '未使用优惠券订单GMV'
+
+
+)
+PARTITIONED BY ( 
+  country_code string COMMENT '国家码',dt string COMMENT '分区时间')
+STORED AS orc 
+LOCATION
+  'oss://opay-datalake/opos/opos_dw/app_opos_metrics_report_mid'
+TBLPROPERTIES ('orc.compress'='SNAPPY');
+
+--insert overwrite table opos_dw.app_opos_report_mid partition (country_code,dt) select * from opos_dw.app_opos_metrcis_report_tmp_d where country_code='nal';
+
+--增加满减和返现列
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(cashback_order_cnt int COMMENT '返现成功订单数');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(cashback_fail_order_cnt int COMMENT '返现失败订单数');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(cashback_order_gmv decimal(20,2) COMMENT '返现订单GMV');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(cashback_per_order_amt decimal(20,2) COMMENT '返现单均价');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(cashback_per_people_amt decimal(20,2) COMMENT '返现客单价');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(cashback_people_cnt int COMMENT '返现客户数量');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(cashback_first_people_cnt int COMMENT '首单用户数（返现）');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(cashback_zero_order_cnt int COMMENT '无返现订单数');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(cashback_order_percent decimal(20,4) COMMENT '返现订单占比');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(cashback_amt decimal(20,2) COMMENT '返现金额');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(reduce_order_cnt int COMMENT '立减成功订单数');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(reduce_zero_order_cnt int COMMENT '无立减订单数量');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(reduce_amt decimal(20,2) COMMENT '立减金额');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(reduce_order_gmv decimal(20,2) COMMENT '立减订单GMV');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(reduce_per_order_amt decimal(20,2) COMMENT '立减单均价');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(reduce_per_people_amt decimal(20,2) COMMENT '立减客单价');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(reduce_people_cnt int COMMENT '参与立减用户数');
+--alter table opos_dw.app_opos_metrcis_report_tmp_d add columns(reduce_first_people_cnt int COMMENT '首单用户数（立减）');
+
+--alter table opos_dw.app_opos_metrics_report_mid add columns(month_newshop_cnt int COMMENT '本月新增商户数');
+
+--优惠券列
+--alter table opos_dw.app_opos_metrics_report_mid add columns(coupon_order_cnt int COMMENT '优惠券订单数量');
+--alter table opos_dw.app_opos_metrics_report_mid add columns(coupon_order_people int COMMENT '用户数（优惠券）');
+--alter table opos_dw.app_opos_metrics_report_mid add columns(coupon_first_order_people int COMMENT '首单用户数（优惠券）');
+--alter table opos_dw.app_opos_metrics_report_mid add columns(coupon_pay_amount decimal(20,2) COMMENT '优惠券订单实付金额');
+--alter table opos_dw.app_opos_metrics_report_mid add columns(coupon_order_gmv decimal(20,2) COMMENT '优惠券订单GMV');
+--alter table opos_dw.app_opos_metrics_report_mid add columns(coupon_discount_amount decimal(20,2) COMMENT '优惠券核销金额');
+
+--alter table opos_dw.app_opos_metrics_report_mid add columns(coupon_useless_order_cnt int COMMENT '未使用优惠订单数');
+--alter table opos_dw.app_opos_metrics_report_mid add columns(coupon_useless_order_people int COMMENT '未使用优惠用户数');
+--alter table opos_dw.app_opos_metrics_report_mid add columns(coupon_useless_pay_amount decimal(20,2)  COMMENT '未使用优惠券订单实付金额');
+--alter table opos_dw.app_opos_metrics_report_mid add columns(coupon_useless_order_gmv decimal(20,2) COMMENT '未使用优惠券订单GMV');
+
+set hive.exec.parallel=true;
+set hive.exec.dynamic.partition.mode=nonstrict;
+
 --01.向report临时表中插入当日销售和订单的数据
 insert overwrite table opos_dw.app_opos_metrics_report_mid partition (country_code,dt)
 select
@@ -150,6 +263,18 @@ nvl(a.hcm_id,b.hcm_id) as hcm_id
 
 ,nvl(a.month_newshop_cnt,0) as month_newshop_cnt
 
+,nvl(b.coupon_order_cnt,0) as coupon_order_cnt
+,nvl(b.coupon_order_people,0) as coupon_order_people
+,nvl(b.coupon_first_order_people,0) as coupon_first_order_people
+,nvl(b.coupon_pay_amount,0) as coupon_pay_amount
+,nvl(b.coupon_order_gmv,0) as coupon_order_gmv
+,nvl(b.coupon_discount_amount,0) as coupon_discount_amount
+
+,nvl(b.coupon_useless_order_cnt,0) as coupon_useless_order_cnt
+,nvl(b.coupon_useless_order_people,0) as coupon_useless_order_people
+,nvl(b.coupon_useless_pay_amount,0) as coupon_useless_pay_amount
+,nvl(b.coupon_useless_order_gmv,0) as coupon_useless_order_gmv
+
 ,'nal' as country_code
 ,'{pt}' as dt
 from
@@ -159,9 +284,9 @@ from
   ,rm_id
   ,bdm_id
   ,bd_id
-
+  
   ,city_code as city_id
-
+  
   ,count(id) as merchant_cnt
   ,0 as pos_merchant_cnt
   ,count(if(created_at = '{pt}',id,null)) as new_merchant_cnt
@@ -177,7 +302,7 @@ from
   ,rm_id
   ,bdm_id
   ,bd_id
-
+  
   ,city_code
   ) as a
 full join
@@ -188,9 +313,9 @@ full join
   ,rm_id
   ,bdm_id
   ,bd_id
-
+  
   ,city_id
-
+  
   ,nvl(count(if(order_type = 'pos',order_id,null)),0) as pos_complete_order_cnt
   ,nvl(count(if(order_type = 'qrcode',order_id,null)),0) as qr_complete_order_cnt
   ,nvl(count(order_id),0) as complete_order_cnt
@@ -200,7 +325,7 @@ full join
   ,nvl(sum(if(first_order = '1',nvl(org_payment_amount,0) - nvl(pay_amount,0) + nvl(user_subsidy,0),0)),0) as new_user_cost
   ,nvl(sum(if(first_order <> '1',nvl(org_payment_amount,0) - nvl(pay_amount,0) + nvl(user_subsidy,0),0)),0) as old_user_cost
   ,nvl(count(if(return_amount > 0,order_id,null)),0) as return_amount_order_cnt
-
+  
   --返现活动情况分析
   ,count(1) as order_cnt
   ,count(if(user_subsidy_status='SUCCESS',1,null)) as cashback_order_cnt
@@ -213,7 +338,7 @@ full join
   ,count(if(user_subsidy=0,1,null)) as cashback_zero_order_cnt
   ,nvl(count(if(user_subsidy_status='SUCCESS',1,null))/count(1),0) as cashback_order_percent
   ,sum(if(user_subsidy>0 and user_subsidy_status='SUCCESS',nvl(user_subsidy,0),0)) as cashback_amt
-
+  
   ,count(if(activity_type in ('RFR','FR'),1,null)) as reduce_order_cnt
   ,count(if(activity_type not in ('RFR','FR'),1,null)) as reduce_zero_order_cnt
   ,sum(if(activity_type in ('RFR','FR'),nvl(discount_amount,0),0)) as reduce_amt
@@ -222,10 +347,23 @@ full join
   ,nvl(sum(if(activity_type in ('RFR','FR'),nvl(org_payment_amount,0),0))/count(distinct(if(activity_type not in ('RFR','FR'),sender_id,null))),0) as reduce_per_people_amt
   ,count(distinct(if(activity_type in ('RFR','FR'),sender_id,null))) as reduce_people_cnt
   ,count(distinct(if(activity_type in ('RFR','FR') and first_order='1',sender_id,null))) as reduce_first_people_cnt
+  
+  --优惠券分析
+  ,count(if(discount_type is not null,1,null)) as coupon_order_cnt
+  ,count(distinct(if(discount_type is not null,sender_id,null))) as coupon_order_people
+  ,count(distinct(if(discount_type is not null and first_order='1',sender_id,null))) as coupon_first_order_people
+  ,sum(if(discount_type is not null,pay_amount,0)) as coupon_pay_amount
+  ,sum(if(discount_type is not null,org_payment_amount,0)) as coupon_order_gmv
+  ,sum(if(discount_type is not null,discount_amount,0)) as coupon_discount_amount
+
+  ,count(if(discount_type is null,1,null)) as coupon_useless_order_cnt
+  ,count(distinct(if(discount_type is null,sender_id,null))) as coupon_useless_order_people
+  ,sum(if(discount_type is null,pay_amount,0)) as coupon_useless_pay_amount
+  ,sum(if(discount_type is null,org_payment_amount,0)) as coupon_useless_order_gmv
 
   ,'nal' as country_code
   ,'{pt}' as dt
-
+  
   from 
   opos_dw.dwd_pre_opos_payment_order_di as p
   where 
@@ -238,7 +376,7 @@ full join
   ,rm_id
   ,bdm_id
   ,bd_id
-
+  
   ,city_id
   ) as b
 on
