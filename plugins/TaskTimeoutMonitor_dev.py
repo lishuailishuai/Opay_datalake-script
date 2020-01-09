4c41b691bb8c0a964766f8336027b54b9569d40b
杨明泽 <mingze.yang@opay-inc.com>
2020-01-09 16:34:20 +0800

    no message

--- plugins/TaskTimeoutMonitor_dev.py
+++ plugins/TaskTimeoutMonitor_dev.py
@@ -91,19 +91,16 @@ class TaskTimeoutMonitor_dev(object):
                 out.close()
     
                 logging.info("数据标识的返回值："+str(res))
-
-                print("----",self.owner_name)
-
-                print(res)
     
                 #判断数据文件是否生成
                 if res == '' or res == 'None' or res == '0':
 
-                    print(res)
-
                     if sum_timeout >= int(timeout):
 
-                        print(sum_timeout)
+                        format_date=timeout/60
+
+                        print(format_date)
+
                         self.dingding_alert.send('DW 【及时性预警】调度任务:{dag_id} 产出超时。负责人:{owner_name}。预留时间: {timeout} 秒'.format(
                                 dag_id=dag_id_name,
                                 timeout=timeout,
