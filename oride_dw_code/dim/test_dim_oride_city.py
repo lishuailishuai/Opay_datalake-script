d0b6d21e70c8d09dc89a4b967e84aca343911c1b
杨明泽 <mingze.yang@opay-inc.com>
2020-01-09 14:07:57 +0800

    no message

--- oride_dw_code/dim/test_dim_oride_city.py
+++ oride_dw_code/dim/test_dim_oride_city.py
@@ -157,7 +157,7 @@ else:
     )
 
 
-    hdfs_path="oss://opay-datalake/oride/oride_dw/"+table_name
+    hdfs_path="oss://opay-datalake/oride/test_db/"+table_name
 
 ##----------------------------------------- 脚本 ---------------------------------------## 
 
@@ -167,7 +167,7 @@ def fun_task_timeout_monitor(ds,dag,**op_kwargs):
     dag_ids=dag.dag_id
 
     tb = [
-        {"dag":dag,"db": "oride_dw", "table":"{dag_name}".format(dag_name=dag_ids), "partition": "country_code=nal/dt={pt}".format(pt=ds), "timeout": "600"}
+        {"dag":dag,"db": "test_db", "table":"{dag_name}".format(dag_name=dag_ids), "partition": "country_code=nal/dt={pt}".format(pt=ds), "timeout": "600"}
     ]
 
     TaskTimeoutMonitor_dev().set_task_monitor(tb)
