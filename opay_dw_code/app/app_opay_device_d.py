8c92673cd5efb7920471b5366ba2e36be745bda5
xiedong <dong.xie@opay-inc.com>
2020-01-11 11:19:46 +0800

    修改

--- opay_dw_code/app/app_opay_device_d.py
+++ opay_dw_code/app/app_opay_device_d.py
@@ -26,7 +26,7 @@ from airflow.sensors import OssSensor
 
 args = {
     'owner': 'lishuai',
-    'start_date': datetime(2020, 1, 2),
+    'start_date': datetime(2020, 1, 10),
     'depends_on_past': False,
     'retries': 3,
     'retry_delay': timedelta(minutes=2),
