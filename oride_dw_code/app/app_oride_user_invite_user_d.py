6897bec11aa8d40268d6413f78fc14770ad0e66a
chenlili <lili.chen@opay-inc.com>
2020-01-14 18:07:03 +0800

    新增dwm层乘客主题表

--- oride_dw_code/app/app_oride_user_invite_user_d.py
+++ oride_dw_code/app/app_oride_user_invite_user_d.py
@@ -159,7 +159,7 @@ def app_oride_user_invite_user_d_sql_task(ds):
     where dt='{pt}'
     and invitee_role=1 
     and role=1) inved_all   --被邀请人
-    on all_user.passenger_id=inved_his.invitee_id
+    on all_user.passenger_id=inved_all.invitee_id
     group by all_user.city_id,
     concat_ws('-',substr(all_user.dt),weekofyear(all_user.dt));
     '''.format(
