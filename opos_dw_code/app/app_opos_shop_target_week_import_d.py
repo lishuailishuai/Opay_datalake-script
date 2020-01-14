6f5eda6e9a5ab92dc7b2c5580a0214bf32603081
yuanfeng <feng.yuan@opay-inc.com>
2020-01-14 17:45:21 +0800

    更新dag

--- opos_dw_code/app/app_opos_shop_target_week_import_d.py
+++ opos_dw_code/app/app_opos_shop_target_week_import_d.py
@@ -73,6 +73,7 @@ def judge_monday(ds, **kargs):
             ds='{{ds}}',
             before_1_day='{{ macros.ds_add(ds, -1) }}'
         )
+
     else:
         delete_sql = """
     DELETE FROM opos_dw.app_opos_shop_target_week_w WHERE dt='{ds}';
@@ -83,6 +84,7 @@ def judge_monday(ds, **kargs):
         )
 
     opos_mysql_cursor.execute(delete_sql)
+    opos_mysql_conn.commit()
 
 judge_monday_task = PythonOperator(
     task_id='judge_monday_task',
@@ -104,109 +106,9 @@ judge_monday_task = PythonOperator(
 
 ##----------------------------------------- 将最新数据插入到mysql ---------------------------------------##
 
-insert_mysql_today_data = HiveToMySqlTransfer(
-    task_id='insert_mysql_today_data',
-    sql="""
-select
-id
-,shop_id
-,opay_id
-,shop_name
-,opay_account
-,create_week
-,city_code
-,city_name
-,country
-,hcm_id
-,hcm_name
-,cm_id
-,cm_name
-,rm_id
-,rm_name
-,bdm_id
-,bdm_name
-,bd_id
-,bd_name
-,order_cnt
-,cashback_order_cnt
-,cashback_fail_order_cnt
-,cashback_order_gmv
-,cashback_per_order_amt
-,cashback_per_people_amt
-,cashback_people_cnt
-,cashback_first_people_cnt
-,cashback_zero_order_cnt
-,cashback_amt
-,reduce_order_cnt
-,reduce_zero_order_cnt
-,reduce_amt
-,reduce_order_gmv
-,reduce_people_cnt
-,reduce_first_people_cnt
-,bonus_order_cnt
-,order_people
-,not_first_order_people
-,first_order_people
-,first_bonus_order_people
-,order_gmv
-,bonus_order_gmv
-,bonus_order_amt
-,sweep_amt
-,bonus_order_people
-,bonus_order_times
-,order_create_cnt
-,order_pay_cnt
-,order_fail_cnt
-,order_pending_cnt
-,coupon_order_cnt
-,coupon_order_people
-,coupon_first_order_people
-,coupon_pay_amount
-,coupon_order_gmv
-,coupon_discount_amount
-,coupon_useless_order_cnt
-,coupon_useless_order_people
-,coupon_useless_pay_amount
-,coupon_useless_order_gmv
-,bak1
-,bak2
-,bak3
-,bak4
-,bak5
-,bak6
-,bak7
-,bak8
-,bak9
-,bak10
-,bak11
-,bak12
-,bak13
-,bak14
-,bak15
-,bak16
-,bak17
-,bak18
-,bak19
-,bak20
-,country_code
-,dt
-from
-opos_dw.app_opos_shop_target_week_w
-where
-country_code = 'nal'
-and dt='{ds}'
-
-    """.format(
-        ds='{{ds}}',
-        before_1_day ='{{ macros.ds_add(ds, -1) }}'
-    ),
-    mysql_conn_id='mysql_dw',
-    mysql_table='app_opos_shop_target_week_w',
-    dag=dag)
-
 ##----------------------------------------- 最后执行流程 ---------------------------------------##
 
-app_opos_shop_target_week_w_task >> judge_monday_task >> insert_mysql_today_data
+app_opos_shop_target_week_w_task >> judge_monday_task
 
 
 
