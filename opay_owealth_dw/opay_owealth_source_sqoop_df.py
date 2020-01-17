a275b8bc7b1ba4496a3fd35c416b3ef19a1bd311
zhangzhenqian <jasonzhang081114@gmail.com>
2020-01-17 11:10:07 +0800

    owealth sqoop change schedule

--- opay_owealth_dw/opay_owealth_source_sqoop_df.py
+++ opay_owealth_dw/opay_owealth_source_sqoop_df.py
@@ -16,7 +16,7 @@ from airflow.models import Variable
 
 args = {
     'owner': 'zhenqian.zhang',
-    'start_date': datetime(2019, 10, 30),
+    'start_date': datetime(2020, 01, 16),
     'depends_on_past': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
