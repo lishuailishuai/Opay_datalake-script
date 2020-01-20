f8ab8467ba77f41c4c247c77ebd170125161dc62
杨明泽 <mingze.yang@opay-inc.com>
2020-01-20 16:59:02 +0800

    no message

--- oride_dw_code/dwd/dwd_oride_algo_driver_score_init_info_di.py
+++ oride_dw_code/dwd/dwd_oride_algo_driver_score_init_info_di.py
@@ -40,7 +40,7 @@ args = {
 dag = airflow.DAG( 'dwd_oride_algo_driver_score_init_info_di', 
     schedule_interval="10 02 * * *", 
     default_args=args,
-    catchup=False) 
+    catchup=true) 
 
 
 ##----------------------------------------- 依赖 ---------------------------------------## 
