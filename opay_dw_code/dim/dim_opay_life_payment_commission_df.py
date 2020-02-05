e770a5183e1e2c610ecb5d17c3eeb2d131519c31
xiedong <dong.xie@opay-inc.com>
2020-02-05 11:20:22 +0800

    修改调度时间

--- opay_dw_code/dim/dim_opay_life_payment_commission_df.py
+++ opay_dw_code/dim/dim_opay_life_payment_commission_df.py
@@ -25,7 +25,7 @@ import os
 
 args = {
     'owner': 'xiedong',
-    'start_date': datetime(2019, 12, 20),
+    'start_date': datetime(2019, 9, 21),
     'depends_on_past': False,
     'retries': 3,
     'retry_delay': timedelta(minutes=2),
