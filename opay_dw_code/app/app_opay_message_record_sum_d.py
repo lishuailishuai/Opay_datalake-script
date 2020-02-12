047a0280e9624b9f1be7919606f8156dc9a39e98
shuzhen.liu <shuzhen.liu@opay-inc.com>
2020-02-12 17:44:52 +0800

    xiiug

--- opay_dw_code/app/app_opay_message_record_sum_d.py
+++ opay_dw_code/app/app_opay_message_record_sum_d.py
@@ -124,7 +124,7 @@ def execution_data_task_id(ds, **kargs):
     第二个参数true: 数据有才生成_SUCCESS false 数据没有也生成_SUCCESS 
 
     """
-    TaskTouchzSuccess().countries_touchz_success(ds, db_name, table_name, hdfs_path, "true", "true")
+    TaskTouchzSuccess().countries_touchz_success(ds, db_name, table_name, hdfs_path, "false", "true")
 
 
 app_opay_message_record_sum_d_task = PythonOperator(
