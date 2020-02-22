986cc1d8f3510cf0674fa545faac26a37b802c03
杨明泽 <mingze.yang@opay-inc.com>
2020-02-22 23:40:48 +0800

    多国家开发

--- plugins/CountriesPublicFrame_dev.py
+++ plugins/CountriesPublicFrame_dev.py
@@ -20,14 +20,13 @@ import os,sys,time
 from plugins.TaskTimeoutMonitor import TaskTimeoutMonitor
 from airflow.utils.trigger_rule import TriggerRule
 from plugins.DingdingAlert import DingdingAlert
+from utils.get_local_time import GetLocalTime
 
 
 
 class CountriesPublicFrame_dev(object):
 
-    def __init__(self,v_is_open,v_ds,v_db_name,v_table_name,v_data_hdfs_path,v_country_partition="true",v_file_type="true",v_hour=None):
-
-        #self.comwx = ComwxApi('wwd26d45f97ea74ad2', 'BLE_v25zCmnZaFUgum93j3zVBDK-DjtRkLisI_Wns4g', '1000011')
+    def __init__(self,v_is_open,v_ds,v_db_name,v_table_name,v_data_hdfs_path,v_country_partition="true",v_file_type="true",v_hour=None,v_frame_type="stand"):
 
         self.dingding_alert = DingdingAlert('https://oapi.dingtalk.com/robot/send?access_token=928e66bef8d88edc89fe0f0ddd52bfa4dd28bd4b1d24ab4626c804df8878bb48')
 
@@ -41,6 +40,9 @@ class CountriesPublicFrame_dev(object):
         self.hour=v_hour
         self.is_open=v_is_open
         self.v_del_flag=0
+
+        #stand:standalone many:many
+        self.frame_type=v_frame_type
 
         self.v_country_code_map=None
 
@@ -318,91 +320,6 @@ class CountriesPublicFrame_dev(object):
 
             sys.exit(1)
 
-
-    #有国家码分区
-    def exist_country_code_data_dir_dev(self,object_task):
-
-        """
-        country_partition:是否有国家分区
-        file_type:是否空文件也生成 success
-            
-        """
-
-        try:
-
-            #获取国家列表
-
-            for country_code_word in self.country_code_list.split(","):
-
-                if country_code_word.lower()=='nal':
-                    country_code_word=country_code_word.lower()
-
-                else:
-                    country_code_word=country_code_word.upper()
-
-
-                #没有小时级分区
-                if self.hour is None:
-
-                    #输出不同国家的数据路径(没有小时级分区)
-                    self.hdfs_data_dir_str=self.data_hdfs_path+"/country_code="+country_code_word+"/dt="+self.ds
-                else:
-
-                    #输出不同国家的数据路径(有小时级分区)
-                    self.hdfs_data_dir_str=self.data_hdfs_path+"/country_code="+country_code_word+"/dt="+self.ds+"/hour="+self.hour
-
-
-                #没有开通多国家业务(国家码默认nal)
-                if self.country_partition.lower()=="true" and self.is_open.lower()=="false":
-
-                    #必须有数据才可以生成Success 文件
-                    if self.file_type.lower()=="true":
-
-                        object_task()
-
-                    #数据为空也生成 Success 文件
-                    if self.file_type.lower()=="false":
-
-                        object_task()
-
-                
-                #开通多国家业务
-                if self.country_partition.lower()=="true" and self.is_open.lower()=="true":
-
-                    
-                    #必须有数据才可以生成Success 文件
-                    if self.file_type.lower()=="true":
-
-                        #删除多国家分区使用
-                        if self.v_del_flag==1:
-
-                            object_task()
-
-                            continue
-
-                        #在必须有数据条件下：国家是nal时，数据可以为空 
-                        if country_code_word=="nal":
-                            self.data_not_file_type_touchz()
-
-                        else:
-
-                            object_task()
-                
-                    #数据为空也生成 Success 文件
-                    if self.file_type.lower()=="false":
-
-                        object_task()
-
-                   
-        except Exception as e:
-
-            #self.dingding_alert.send('DW调度系统任务 {jobname} 数据产出异常'.format(jobname=table_name))
-
-            logging.info(e)
-
-            sys.exit(1)
-
-
     def exist_country_code_data_dir(self,object_task):
 
         """
@@ -512,7 +429,7 @@ class CountriesPublicFrame_dev(object):
 
             return alter_str
             
-        # 有国家分区 && 小时参数不为None
+        # 没有国家分区 && 小时参数不为None
         if self.country_partition.lower()=="false" and self.hour is not None:
 
             v_par_str="dt='{ds}',hour='{hour}'".format(ds=self.ds,hour=self.hour)
@@ -521,6 +438,7 @@ class CountriesPublicFrame_dev(object):
 
             return alter_str
 
+        #有国家分区
         for country_code_word in self.country_code_list.split(","):
 
             # 有国家分区 && 小时参数为None
@@ -530,13 +448,27 @@ class CountriesPublicFrame_dev(object):
 
                 alter_str=alter_str+"\n"+"alter table {db}.{table_name} drop partition({v_par});\n alter table {db}.{table_name} add partition({v_par});".format(v_par=v_par_str,table_name=self.table_name,db=self.db_name)
 
-            # 有国家分区 && 小时参数不为None
-            if self.country_partition.lower()=="true" and self.hour is not None:
+            # 有国家分区 && 小时参数不为None && 单国家
+            if self.country_partition.lower()=="true" and self.hour is not None and self.frame_type=="stand":
 
                 v_par_str="country_code='{country_code}',dt='{ds}',hour='{hour}'".format(ds=self.ds,hour=self.hour,country_code=country_code_word)
 
                 alter_str=alter_str+"\n"+"alter table {db}.{table_name} drop partition({v_par});\n alter table {db}.{table_name} add partition({v_par});".format(v_par=v_par_str,table_name=self.table_name,db=self.db_name)
             
+            # 有国家分区 && 小时参数不为None && 多国家
+            if self.country_partition.lower()=="true" and self.hour is not None and self.frame_type=="many":
+
+                print("+++++++")
+                print(country_code_word)
+                print(self.ds+" "+self.hour)
+
+                print("------")
+
+                print(GetLocalTime('{}'.format(v_utc_time=self.ds+" "+self.hour),country_code_word,-1))
+
+                v_par_str="country_code='{country_code}',dt='{ds}',hour='{hour}'".format(ds=self.ds,hour=self.hour,country_code=country_code_word)
+
+                alter_str=alter_str+"\n"+"alter table {db}.{table_name} drop partition({v_par});\n alter table {db}.{table_name} add partition({v_par});".format(v_par=v_par_str,table_name=self.table_name,db=self.db_name)
 
         return alter_str
 
