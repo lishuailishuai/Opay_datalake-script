d2a7ed7fc794465e72220a307a967c61ce40c47e
杨明泽 <mingze.yang@opay-inc.com>
2020-02-23 14:36:05 +0800

    添加备注

--- utils/get_local_time.py
+++ utils/get_local_time.py
@@ -4,10 +4,14 @@ from ast import literal_eval
 import datetime
 
 def GetLocalTime(utc_date_hour, country_code, gap_hour):
-    print(utc_date_hour)
+    #读取配置文件
     config = literal_eval(Variable.get("utc_locale_time_config"))
+
+    #读取国家码对应的[time_zone]
     time_zone = config[country_code]['time_zone']
     time_obj = datetime.datetime.strptime(utc_date_hour, "%Y-%m-%d %H")
+
+    #通过偏移量计算本地时间
     time_obj1 = time_obj + datetime.timedelta(hours=(time_zone+gap_hour))
     re = {
         'date':time_obj1.strftime('%Y-%m-%d'),
