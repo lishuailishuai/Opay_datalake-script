d3c5c191736e0d4bc979615cd9b819d8d174f133
chenlili <lili.chen@opay-inc.com>
2020-02-24 17:16:13 +0800

    新增dwm层乘客主题表

---
+++ ocredit_phones_dw_code/app/app_ocredit_phones_order_base_cube_d.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+import airflow
+from datetime import datetime, timedelta
+from airflow.operators.hive_operator import HiveOperator
+from airflow.operators.impala_plugin import ImpalaOperator
+from utils.connection_helper import get_hive_cursor
+from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.hooks.redis_hook import RedisHook
+from airflow.hooks.hive_hooks import HiveCliHook, HiveServer2Hook
+from airflow.operators.hive_to_mysql import HiveToMySqlTransfer
+from airflow.operators.mysql_operator import MySqlOperator
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
+from airflow.operators.bash_operator import BashOperator
+from airflow.sensors.named_hive_partition_sensor import NamedHivePartitionSensor
+from airflow.sensors.hive_partition_sensor import HivePartitionSensor
+from airflow.sensors import UFileSensor
+from plugins.TaskTimeoutMonitor import TaskTimeoutMonitor
+from plugins.TaskTouchzSuccess import TaskTouchzSuccess
+from plugins.CountriesPublicFrame import CountriesPublicFrame
+import json
+import logging
+from airflow.models import Variable
+import requests
+import os
+from airflow.sensors import OssSensor
+
+args = {
+    'owner': 'lili.chen',
+    'start_date': datetime(2020, 2, 23),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=2),
+    'email': ['bigdata_dw@opay-inc.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+}
+
+dag = airflow.DAG('app_ocredit_phones_order_base_cube_d',
+                  schedule_interval="30 02 * * *",
+                  default_args=args,
+                  catchup=False)
+
+##----------------------------------------- 依赖 ---------------------------------------##
+
+dwd_ocredit_phones_order_base_di_task = OssSensor(
+    task_id='dwd_ocredit_phones_order_base_di_task',
+    bucket_key="{hdfs_path_str}/dt={pt}/_SUCCESS".format(
+        hdfs_path_str="ocredit_phones/ocredit_phones_dw/dwd_ocredit_phones_order_base_di/country_code=nal",
+
+        pt="{{ds}}"
+    ),
+    bucket_name='opay-datalake',
+    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
+    dag=dag
+)
+
+##----------------------------------------- 变量 ---------------------------------------##
+
+db_name = "ocredit_phones_dw"
+table_name = "app_ocredit_phones_order_base_cube_d"
+hdfs_path = "oss://opay-datalake/ocredit_phones/ocredit_phones_dw/" + table_name
+
+
+##----------------------------------------- 任务超时监控 ---------------------------------------##
+
+def fun_task_timeout_monitor(ds, dag, **op_kwargs):
+    dag_ids = dag.dag_id
+
+    msg = [
+        {"dag": dag, "db": "ocredit_phones_dw", "table": "{dag_name}".format(dag_name=dag_ids),
+         "partition": "country_code=nal/dt={pt}".format(pt=ds), "timeout": "800"}
+    ]
+
+    TaskTimeoutMonitor().set_task_monitor(msg)
+
+
+task_timeout_monitor = PythonOperator(
+    task_id='task_timeout_monitor',
+    python_callable=fun_task_timeout_monitor,
+    provide_context=True,
+    dag=dag
+)
+
+
+##----------------------------------------- 脚本 ---------------------------------------##
+
+def app_ocredit_phones_order_base_cube_d_sql_task(ds):
+    HQL = '''
+
+
+    set hive.exec.parallel=true;
+    set hive.exec.dynamic.partition.mode=nonstrict;
+
+    INSERT overwrite TABLE ocredit_phones_dw.{table} partition(country_code,dt)
+
+    select terms,--分期数
+          date_of_entry,--进件日期
+          weekofyear(date_of_entry) as week_of_entry, --进件日期所在周
+          month(date_of_entry) as month_of_entry, --进件所在月
+          count(distinct opay_id) as entry_cnt, --进件量
+          count(distinct (if(order_status='81',opay_id,null))) as loan_cnt, --`放款数` ,
+          sum(if(order_status='81',(nvl(loan_amount,0)/100)*0.2712/100,0)) as loan_amount_usd,--`贷款金额_USD` ,
+          '{pt}' as dt,
+          'nal' as country_code
+    from ocredit_phones_dw.dwd_ocredit_phones_order_di
+    where dt='{pt}' and date_of_entry=dt
+    group by terms,date_of_entry,weekofyear(date_of_entry),month(date_of_entry)
+    grouping sets(date_of_entry,
+          weekofyear(date_of_entry),
+          month(date_of_entry),
+          (terms,date_of_entry),
+          (terms,weekofyear(date_of_entry)),
+          (terms,month(date_of_entry)));
+        '''.format(
+        pt=ds,
+        table=table_name,
+        db=db_name
+    )
+    return HQL
+
+
+# 主流程
+def execution_data_task_id(ds, **kwargs):
+    v_date = kwargs.get('v_execution_date')
+    v_day = kwargs.get('v_execution_day')
+    v_hour = kwargs.get('v_execution_hour')
+
+    hive_hook = HiveCliHook()
+
+    """
+        #功能函数
+        alter语句: alter_partition
+        删除分区: delete_partition
+        生产success: touchz_success
+
+        #参数
+        第一个参数true: 数据目录是有country_code分区。false 没有
+        第二个参数true: 数据有才生成_SUCCESS false 数据没有也生成_SUCCESS 
+
+        #读取sql
+        %_sql_task(ds,v_hour)
+
+        第一个参数ds: 天级任务
+        第二个参数v_hour: 小时级任务，需要使用
+
+    """
+
+    cf = CountriesPublicFrame("false", ds, db_name, table_name, hdfs_path, "true", "true")
+
+    # 删除分区
+    # cf.delete_partition()
+
+    # 拼接SQL
+
+    _sql = "\n" + cf.alter_partition() + "\n" + app_ocredit_phones_order_base_cube_d_sql_task(ds)
+
+    logging.info('Executing: %s', _sql)
+
+    # 执行Hive
+    hive_hook.run_cli(_sql)
+
+    # 熔断数据，如果数据不能为0
+    # check_key_data_cnt_task(ds)
+
+    # 熔断数据
+    # check_key_data_task(ds)
+
+    # 生产success
+    cf.touchz_success()
+
+
+app_ocredit_phones_order_base_cube_d_task = PythonOperator(
+    task_id='app_ocredit_phones_order_base_cube_d_task',
+    python_callable=execution_data_task_id,
+    provide_context=True,
+    op_kwargs={
+        'v_execution_date': '{{execution_date.strftime("%Y-%m-%d %H:%M:%S")}}',
+        'v_execution_day': '{{execution_date.strftime("%Y-%m-%d")}}',
+        'v_execution_hour': '{{execution_date.strftime("%H")}}'
+    },
+    dag=dag
+)
+
+dwd_ocredit_phones_order_base_di_task >> app_ocredit_phones_order_base_cube_d_task
