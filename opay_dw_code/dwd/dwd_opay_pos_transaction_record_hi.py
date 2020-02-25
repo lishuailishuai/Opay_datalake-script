f7aa4bb30fdd646793c0bbee3316d354460e8077
shuzhen.liu <shuzhen.liu@opay-inc.com>
2020-02-25 19:11:26 +0800

    xiug

--- opay_dw_code/dwd/dwd_opay_pos_transaction_record_hi.py
+++ opay_dw_code/dwd/dwd_opay_pos_transaction_record_hi.py
@@ -25,6 +25,8 @@ import logging
 from airflow.models import Variable
 import requests
 import os
+from utils.get_local_time import GetLocalTime
+
 
 args = {
     'owner': 'yuanfeng',
@@ -125,6 +127,33 @@ ods_binlog_base_merchant_pos_transaction_record_hi_check_task = OssSensor(
 
 
 ##----------------------------------------- 任务超时监控 ---------------------------------------##
+##----------------------------------------- 任务超时监控 ---------------------------------------##
+def fun_task_timeout_monitor(ds,dag,execution_date,**op_kwargs):
+
+    dag_ids=dag.dag_id
+
+    #监控国家
+    v_country_code='NG'
+
+    #时间偏移量
+    v_gap_hour=0
+
+    v_date=GetLocalTime(execution_date.strftime("%Y-%m-%d %H"),v_country_code,v_gap_hour)['date']
+    v_hour=GetLocalTime(execution_date.strftime("%Y-%m-%d %H"),v_country_code,v_gap_hour)['hour']
+
+    #小时级监控
+    tb_hour_task = [
+        {"dag":dag,"db": "opay_dw", "table":"{dag_name}".format(dag_name=dag_ids), "partition": "country_code={country_code}/dt={pt}/hour={now_hour}".format(country_code=v_country_code,pt=v_date,now_hour=v_hour), "timeout": "100"}
+    ]
+
+    TaskTimeoutMonitor().set_task_monitor(tb_hour_task)
+
+task_timeout_monitor= PythonOperator(
+    task_id='task_timeout_monitor',
+    python_callable=fun_task_timeout_monitor,
+    provide_context=True,
+    dag=dag
+)
 
 
 def dwd_opay_pos_transaction_record_hi_sql_task(ds, v_date):
