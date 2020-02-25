dcdc00ff146e5f8746c16b266349794d53962053
xiedong <dong.xie@opay-inc.com>
2020-02-25 20:34:12 +0800

    修改

--- opay_dw_code/dim/dim_opay_user_base_hf.py
+++ opay_dw_code/dim/dim_opay_user_base_hf.py
@@ -144,7 +144,7 @@ def dim_opay_user_base_hf_sql_task(ds, v_date):
                 photo,
                 big_picture,
                 nick_name,
-                row_number() over(partition by order_no order by `__ts_ms` desc,`__file` desc,cast(`__pos` as int) desc) rn
+                row_number() over(partition by user_id order by `__ts_ms` desc,`__file` desc,cast(`__pos` as int) desc) rn
             from opay_dw_ods.ods_binlog_base_user_hi 
             where concat(dt, " ", hour) = date_format('{v_date}', 'yyyy-MM-dd HH') and `__deleted` = 'false'
     )
