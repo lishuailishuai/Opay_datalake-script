caab3b4e31c43ee90caff02788883b4d536873f3
zhangzhenqian <jasonzhang081114@gmail.com>
2020-02-26 17:32:21 +0800

    app_opay_life_payment_sum_ng_h_to_mysql

--- opay_dw_code/app/app_opay_life_payment_sum_ng_h_to_mysql.py
+++ opay_dw_code/app/app_opay_life_payment_sum_ng_h_to_mysql.py
@@ -204,13 +204,17 @@ def __data_to_mysql(conn, data, column, update=''):
                 sval += ',(\'{}\')'.format('\',\''.join([str(x) for x in row]))
             cnt += 1
             if cnt >= 1000:
-                # logging.info(esql.format(isql, sval, update))
+                logging.info(esql.format(isql, sval, update))
+                logging.info('mysql insert rows:%d', cnt)
                 conn.execute(esql.format(isql, sval, update))
                 cnt = 0
                 sval = ''
 
         if cnt > 0 and sval != '':
+            logging.info(esql.format(isql, sval, update))
+            logging.info('mysql insert rows:%d', cnt)
             conn.execute(esql.format(isql, sval, update))
+
     except BaseException as e:
         logging.info(e)
         sys.exit(1)
