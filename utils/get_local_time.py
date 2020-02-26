e29b0aaef44ab8ad89277b1d67a5d85f1a48c55d
杨明泽 <mingze.yang@opay-inc.com>
2020-02-26 10:49:08 +0800

    no message

--- utils/get_local_time.py
+++ utils/get_local_time.py
@@ -3,9 +3,22 @@ from airflow.models import Variable
 from ast import literal_eval
 import datetime
 
-def GetLocalTime(utc_date_hour, country_code, gap_hour):
+
+"""
+    service_line="opay" --业务线
+    country_code = 'NG' --二位国家码
+    utc_date_hour = '2020-02-15 23' --调度执行时间(utc)
+    gap_hour=0  --小时偏移量
+ 
+"""
+
+
+def GetLocalTime(service_line,utc_date_hour, country_code, gap_hour):
+
+    service_line_config_file=service_line.lower().strip()+"_time_zone_config"
+
     #读取配置文件
-    config = literal_eval(Variable.get("utc_locale_time_config"))
+    config = literal_eval(Variable.get(service_line_config_file))
 
     #读取国家码对应的[time_zone]
     time_zone = config[country_code]['time_zone']
@@ -19,8 +32,3 @@ def GetLocalTime(utc_date_hour, country_code, gap_hour):
     }
     return re
  
-# country_code = 'NG'
-# utc_date_hour = '2020-02-15 23'
-# gap_hour=0
-# d = GetLocalTime(utc_date_hour, country_code, gap_hour)
-# print(d)
