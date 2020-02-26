19d15accf40f2d7f66e7e9ec51e1feb312138a02
needjuju <39019332@qq.com>
2020-02-26 20:09:12 +0800

    app_opay_cico_sum_ng_h
    Summary: y
    
    Test Plan: y
    
    Reviewers: lili.chen
    
    Differential Revision: https://pha.static.kunlun.com/D21522

--- opay_dw_code/app/app_opay_pos_sum_ng_h_to_mysql.py
+++ opay_dw_code/app/app_opay_pos_sum_ng_h_to_mysql.py
@@ -107,7 +107,7 @@ def get_data_from_hive(ds, execution_date, **op_kwargs):
             dt, 
             hour 
 
-        from  oride_dw.app_opay_pos_sum_ng_h
+        from  opay_dw.app_opay_pos_sum_ng_h
         where 
         country_code = 'NG'
     and concat(dt,' ',hour) >= date_format(default.localTime("{config}", 'NG', '{v_date}', -1), 'yyyy-MM-dd HH')
