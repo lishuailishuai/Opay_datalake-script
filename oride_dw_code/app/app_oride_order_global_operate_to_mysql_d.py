19766e390feeee0f6298a221a68a7ec34d16605f
needjuju <39019332@qq.com>
2020-03-02 16:35:42 +0800

    c 端补贴
    Summary: y
    
    Test Plan: y
    
    Reviewers: lili.chen
    
    Differential Revision: https://pha.static.kunlun.com/D21660

--- oride_dw_code/app/app_oride_order_global_operate_to_mysql_d.py
+++ oride_dw_code/app/app_oride_order_global_operate_to_mysql_d.py
@@ -438,7 +438,7 @@ insert overwrite table oride_dw.{table} partition(country_code,dt)
                select 
                 city_id,
 
-                sum(if(dt ='{pt}',nvl(recharge_amount,0) + nvl(reward_amount,0),0) as b_subsidy_d,--B端补贴、天(实际b补)
+                sum(if(dt ='{pt}',nvl(recharge_amount,0) + nvl(reward_amount,0),0)) as b_subsidy_d,--B端补贴、天(实际b补)
 
                 sum(nvl(recharge_amount,0)) + sum(nvl(reward_amount,0)) as b_subsidy_m--B端补贴 月
 
