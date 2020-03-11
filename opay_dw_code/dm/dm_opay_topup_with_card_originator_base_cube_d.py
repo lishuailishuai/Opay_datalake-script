760882699d5e5156844fe4c8ba77dbf8ab8b8562
xiedong <dong.xie@opay-inc.com>
2020-03-11 17:12:33 +0800

    修改

--- opay_dw_code/dm/dm_opay_topup_with_card_originator_base_cube_d.py
+++ opay_dw_code/dm/dm_opay_topup_with_card_originator_base_cube_d.py
@@ -97,7 +97,7 @@ def dm_opay_topup_with_card_originator_base_cube_d_sql_task(ds):
             amount
         from {db}.dwd_opay_topup_with_card_record_di
         where dt = '{pt}' 
-            and create_time BETWEEN date_format(date_sub('{pt}', 1), 'yyyy-MM-dd 23') AND date_format('{pt}', 'yyyy-MM-dd 23')
+            and date_format(create_time, 'yyyy-MM-dd') = dt
     ) t1
     group by country_code, out_channel_id, originator_type, originator_role, order_status
     with cube
