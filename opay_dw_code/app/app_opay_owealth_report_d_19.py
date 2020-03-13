48c0cea7e743f123faedf5efce909954c39ee3bf
shuzhen.liu <shuzhen.liu@opay-inc.com>
2020-03-13 17:04:10 +0800

    xiug

--- opay_dw_code/app/app_opay_owealth_report_d_19.py
+++ opay_dw_code/app/app_opay_owealth_report_d_19.py
@@ -27,7 +27,7 @@ import os
 
 args = {
     'owner': 'liushuzhen',
-    'start_date': datetime(2019, 12, 20),
+    'start_date': datetime(2020, 3, 12),
     'depends_on_past': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
@@ -37,22 +37,11 @@ args = {
 }
 
 dag = airflow.DAG(
-    'app_opay_owealth_collect_d',
+    'app_opay_owealth_report_d_19',
     schedule_interval="00 18 * * *",
     default_args=args)
 
 ##----------------------------------------- 依赖 ---------------------------------------##
-ods_sqoop_owealth_share_acct_hf_prev_day_task = OssSensor(
-    task_id='ods_sqoop_owealth_share_acct_hf_prev_day_task',
-    bucket_key='{hdfs_path_str}/dt={pt}/hour=17/_SUCCESS'.format(
-        hdfs_path_str="opay_owealth_sqoop_hf/opay_owealth/share_acct",
-        pt='{{macros.ds_add(ds, +1)}}'
-    ),
-    bucket_name='opay-datalake',
-    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
-    dag=dag
-)
-
 ods_sqoop_owealth_share_order_hf_prev_day_task = OssSensor(
     task_id='ods_sqoop_owealth_share_order_hf_prev_day_task',
     bucket_key='{hdfs_path_str}/dt={pt}/hour=17/_SUCCESS'.format(
@@ -74,11 +63,10 @@ ods_sqoop_base_owealth_user_subscribed_hf_prev_day_task = OssSensor(
     poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
     dag=dag
 )
-
-ods_sqoop_owealth_share_revenue_log_hf_prev_day_task = OssSensor(
-    task_id='ods_sqoop_owealth_share_revenue_log_hf_prev_day_task',
+ods_sqoop_base_owealth_share_trans_record_hf_prev_day_task = OssSensor(
+    task_id='ods_sqoop_base_owealth_share_trans_record_hf_prev_day_task',
     bucket_key='{hdfs_path_str}/dt={pt}/hour=17/_SUCCESS'.format(
-        hdfs_path_str="opay_owealth_sqoop_hf/opay_owealth/share_revenue_log",
+        hdfs_path_str="opay_owealth_sqoop_hf/opay_owealth/share_trans_record",
         pt='{{macros.ds_add(ds, +1)}}'
     ),
     bucket_name='opay-datalake',
@@ -86,6 +74,16 @@ ods_sqoop_owealth_share_revenue_log_hf_prev_day_task = OssSensor(
     dag=dag
 )
 
+ods_sqoop_base_user_hf_prev_day_task = OssSensor(
+    task_id='ods_sqoop_base_user_hf_prev_day_task',
+    bucket_key='{hdfs_path_str}/dt={pt}/hour=18/_SUCCESS'.format(
+        hdfs_path_str="opay_dw_sqoop_hf/opay_user/user",
+        pt='{{macros.ds_add(ds, +1)}}'
+    ),
+    bucket_name='opay-datalake',
+    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
+    dag=dag
+)
 
 ##----------------------------------------- 任务超时监控 ---------------------------------------##
 def fun_task_timeout_monitor(ds, dag, **op_kwargs):
@@ -108,113 +106,118 @@ task_timeout_monitor = PythonOperator(
 
 ##----------------------------------------- 变量 ---------------------------------------##
 db_name = "opay_dw"
-table_name = "app_opay_owealth_collect_d"
+table_name = "app_opay_owealth_report_d_19"
 hdfs_path = "oss://opay-datalake/opay/opay_dw/" + table_name
 
 
-def app_opay_owealth_collect_d_sql_task(ds):
+def app_opay_owealth_report_d_19_sql_task(ds):
     HQL = '''
 
     set mapred.max.split.size=1000000;
-    WITH acct_base AS
-  (SELECT user_id,
-          create_time,
-          balance
-   FROM opay_owealth_ods.ods_sqoop_owealth_share_acct_hf
-   WHERE dt='{pt}'
-     AND hour='17'
-     AND from_unixtime(unix_timestamp(create_time, 'yyyy-MM-dd HH:mm:ss')+3600)<'{pt} 19:00:00' ),
-     order_base AS
-  (SELECT create_time,
-          order_type,
-          trans_amount,
-          user_id,
-          memo
-   FROM opay_owealth_ods.ods_sqoop_owealth_share_order_hf
-   WHERE dt='{pt}'
-     AND hour='17'
-     AND status="S"
-     AND from_unixtime(unix_timestamp(create_time, 'yyyy-MM-dd HH:mm:ss')+3600)>='{yesterday} 19:00:00'
-     AND from_unixtime(unix_timestamp(create_time, 'yyyy-MM-dd HH:mm:ss')+3600)<'{pt} 19:00:00' ),
-     user_subscribed AS
-  (SELECT user_id,
+    WITH 
+    order_base AS
+       (SELECT create_time,
+               order_type,
+               trans_amount,
+               user_id,
+               memo
+       FROM opay_owealth_ods.ods_sqoop_owealth_share_order_hf
+       WHERE dt='{pt}'
+          AND hour='17'
+          AND status="S"
+          AND create_time>='{yesterday} 19:00:00'
+          AND create_time<'{pt} 19:00:00' 
+       ),
+    user_subscribed AS
+       (SELECT user_id,
           update_time,
-          subscribed
-   FROM opay_dw_ods.ods_sqoop_base_owealth_user_subscribed_hf
-   WHERE dt='{pt}'
-     AND hour='18'
-     AND from_unixtime(unix_timestamp(update_time, 'yyyy-MM-dd HH:mm:ss')+3600)<'{pt} 19:00:00' ),
-     revenue AS
-  (SELECT *
-   FROM opay_owealth_ods.ods_sqoop_owealth_share_revenue_log_hf
-   WHERE dt='{pt}'
-     AND hour='17'
-     AND from_unixtime(unix_timestamp(revenue_date, 'yyyy-MM-dd HH:mm:ss')+3600)>='{yesterday} 19:00:00' 
-    AND from_unixtime(unix_timestamp(revenue_date, 'yyyy-MM-dd HH:mm:ss')+3600)<'{pt} 19:00:00' ) 
-INSERT overwrite TABLE opay_dw.app_opay_owealth_collect_d partition (country_code='NG',dt='{pt}')
+          subscribed,
+          mobile
+        FROM opay_dw_ods.ods_sqoop_base_owealth_user_subscribed_hf
+        WHERE dt='{pt}'
+           AND hour='18'
+           AND create_time<'{pt} 19:00:00' 
+            ),
+    share_trans_record AS
+       (SELECT order_type,
+               amount,
+               user_id,
+               create_time
+        FROM opay_owealth_ods.ods_sqoop_owealth_share_trans_record_hf
+        WHERE dt='{pt}'
+           and hour='17'
+          AND create_time<'{pt} 19:00:00' 
+        ),
+    user_role AS
+       (SELECT user_id,
+              ROLE,
+              mobile
+        FROM opay_dw_ods.ods_sqoop_base_user_hf 
+        where dt='{pt}'
+           and hour='18'
+           and create_time<'{pt} 19:00:00' 
+         )
+       
+    --INSERT overwrite TABLE opay_dw.app_opay_owealth_report_d_19 partition (country_code='NG',dt='{pt}')
+    SELECT
+         balance, --余额（总的申购-总的赎回+总的利息）
+         curr_subscribe_amount,--当天申购金额
+         curr_subscribe_cnt,--当天申购笔数
+         curr_redeem_amount,--当天赎回金额
+         curr_redeem_cnt,--当天赎回笔数
+         no_api_subscribe_amount,--当天手动申购金额
+         no_api_subscribe_cnt,--当天手动申购笔数
+         api_subscribe_amount,--当天自动申购金额
+         api_subscribe_cnt,--当天自动申购笔数
+         no_api_subscribe_user,--当天手动申购交易用户数
+         api_subscribe_user,--当天自动申购交易用户数
+         redeem_user,--当天赎回交易用户数
+         add_open_api_subscribe_user,--累计开通自动申购用户数
+         open_api_subscribe_user,--当天开通自动申购用户数
+         close_api_subscribe_user,--当天关闭自用申购用户数
+         curr_revenue_amount, --当天入账利息
+         m.ROLE
 
-SELECT total_balance, --总的累计金额
- total_subscribe_amount,--总的申购金额
- total_redeem_amount,--总的赎回金额
- no_api_subscribe_amount,--总的手动申购金额
- api_subscribe_amount,--总的自动申购金额
- no_api_subscribe_user,--手动申购交易用户数
- api_subscribe_user,--自动申购交易用户数
- redeem_user,--赎回交易用户数
- add_open_api_subscribe_user,--累计开通自动申购用户数
- open_api_subscribe_user,--当天开通自动申购用户数
- close_api_subscribe_user,--当天关闭自用申购用户数
- revenue_amount --入账利息
+    FROM
+      (SELECT '{pt}' AS dt,
+              nvl(ROLE,'ALL') role,
+              sum(case when order_type='1001' then amount end)-sum(case when order_type='1002' then amount end)
+              +sum(case when order_type='1003' then amount end) balance,
+              sum(case when create_time>'{yesterday} 19:00:00' and order_type='1001' then amount end) curr_subscribe_amount,
+              sum(case when create_time>'{yesterday} 19:00:00' and order_type='1001' then 1 end) curr_subscribe_cnt,
+              sum(case when create_time>'{yesterday} 19:00:00' and order_type='1002' then amount end) curr_redeem_amount,
+              sum(case when create_time>'{yesterday} 19:00:00' and order_type='1002' then 1 end) curr_redeem_cnt,
+              sum(case when create_time>'{yesterday} 19:00:00' and order_type='1003' then amount end) curr_revenue_amount
+       FROM share_trans_record a
+       INNER JOIN user_role b ON a.user_id=b.mobile
+       GROUP BY b.ROLE
+       with cube) m
+    LEFT JOIN
+      (SELECT '{pt}' AS dt,
+              nvl(ROLE,'ALL') role,
+              sum(CASE WHEN order_type='1001' AND memo='申购' THEN trans_amount END) no_api_subscribe_amount,
+              sum(CASE WHEN order_type='1001' AND memo='申购' THEN 1 END) no_api_subscribe_cnt,
+              sum(CASE WHEN order_type='1001' AND memo='API' THEN trans_amount END) api_subscribe_amount,
+              sum(CASE WHEN order_type='1001' AND memo='API' THEN 1 END) api_subscribe_cnt,
+              count(DISTINCT CASE WHEN memo='申购' THEN a.user_id END) no_api_subscribe_user,
+              count(DISTINCT CASE WHEN memo='API' THEN a.user_id END) api_subscribe_user,
+              count(DISTINCT CASE WHEN memo='赎回' THEN a.user_id END) redeem_user
+       FROM order_base a
+       INNER JOIN user_role b ON a.user_id=b.mobile
+       GROUP BY ROLE
+       with cube)m1 ON m.dt=m1.dt and m.ROLE=m1.role
+    LEFT JOIN
+      (SELECT '{pt}' AS dt,
+              nvl(role,'ALL') role,
+              count(DISTINCT CASE WHEN subscribed='Y' THEN a.user_id END) add_open_api_subscribe_user,
+              count(DISTINCT CASE WHEN update_time>'{yesterday} 19:00:00' and subscribed='Y' THEN a.user_id END)open_api_subscribe_user,
+              count(DISTINCT CASE WHEN update_time>'{yesterday} 19:00:00' and subscribed='N' THEN a.user_id END) close_api_subscribe_user
+       FROM user_subscribed a
+       INNER JOIN user_role b ON a.mobile=b.mobile
+       GROUP BY ROLE
+       with cube) m2 ON m.dt=m2.dt and m.ROLE=m2.ROLE
+    
 
-FROM
-  (SELECT '{pt}' AS dt,
-          sum(balance) total_balance
-   FROM acct_base a) m
-LEFT JOIN
-  (SELECT '{pt}' AS dt,
-          sum(CASE
-                  WHEN order_type='1001' THEN trans_amount
-              END) total_subscribe_amount,
-          sum(CASE
-                  WHEN order_type='1001'
-                       AND memo='申购' THEN trans_amount
-              END) no_api_subscribe_amount,
-          sum(CASE
-                  WHEN order_type='1001'
-                       AND memo='API' THEN trans_amount
-              END) api_subscribe_amount,
-          count(DISTINCT CASE
-                             WHEN memo='申购' THEN user_id
-                         END) no_api_subscribe_user,
-          count(DISTINCT CASE
-                             WHEN memo='API' THEN user_id
-                         END) api_subscribe_user,
-          sum(CASE
-                  WHEN order_type='1002' and memo='赎回' THEN trans_amount
-              END) total_redeem_amount,
-          count(DISTINCT CASE
-                             WHEN memo='赎回' THEN user_id
-                         END) redeem_user
-   FROM order_base a)m1 ON m.dt=m1.dt
-LEFT JOIN
-  (SELECT '{pt}' AS dt,
-          count(DISTINCT CASE
-                                 WHEN subscribed='Y' THEN user_id end) add_open_api_subscribe_user
-   FROM user_subscribed) m2 ON m.dt=m2.dt
-LEFT JOIN
-  (SELECT '{pt}' AS dt,
-          count(DISTINCT CASE
-                             WHEN subscribed='Y' THEN user_id
-                         END)open_api_subscribe_user,
-          count(DISTINCT CASE
-                             WHEN subscribed='N' THEN user_id
-                         END) close_api_subscribe_user
-   FROM user_subscribed
-   WHERE from_unixtime(unix_timestamp(update_time, 'yyyy-MM-dd HH:mm:ss')+3600)>='{yesterday} 19:00:00' ) m3 ON m.dt=m3.dt
-LEFT JOIN
-  (SELECT '{pt}' AS dt,
-          sum(revenue_amount) revenue_amount
-   FROM revenue) m4 ON m.dt=m4.dt
 
 
 '''.format(
@@ -231,7 +234,7 @@ def execution_data_task_id(ds, **kargs):
     hive_hook = HiveCliHook()
 
     # 读取sql
-    _sql = app_opay_owealth_collect_d_sql_task(ds)
+    _sql = app_opay_owealth_report_d_19_sql_task(ds)
 
     logging.info('Executing: %s', _sql)
 
@@ -248,8 +251,8 @@ def execution_data_task_id(ds, **kargs):
                                                  "true")
 
 
-app_opay_owealth_collect_d_task = PythonOperator(
-    task_id='app_opay_owealth_collect_d_task',
+app_opay_owealth_report_d_19_task = PythonOperator(
+    task_id='app_opay_owealth_report_d_19_task',
     python_callable=execution_data_task_id,
     provide_context=True,
     dag=dag
@@ -274,7 +277,7 @@ def send_owealth_report_email(ds, **kwargs):
            format_number(revenue_amount/100,2)
 
         FROM
-           opay_dw.app_opay_owealth_collect_d
+           opay_dw.app_opay_owealth_report_d_19
         WHERE
             dt <= '{dt}'
         ORDER BY dt DESC
@@ -417,10 +420,10 @@ send_owealth_report = PythonOperator(
     provide_context=True,
     dag=dag
 )
-ods_sqoop_owealth_share_acct_hf_prev_day_task >> app_opay_owealth_collect_d_task
-ods_sqoop_owealth_share_order_hf_prev_day_task >> app_opay_owealth_collect_d_task
-ods_sqoop_base_owealth_user_subscribed_hf_prev_day_task >> app_opay_owealth_collect_d_task
-ods_sqoop_owealth_share_revenue_log_hf_prev_day_task >> app_opay_owealth_collect_d_task
-app_opay_owealth_collect_d_task >> send_owealth_report
+ods_sqoop_base_owealth_share_trans_record_hf_prev_day_task >> app_opay_owealth_report_d_19_task
+ods_sqoop_owealth_share_order_hf_prev_day_task >> app_opay_owealth_report_d_19_task
+ods_sqoop_base_owealth_user_subscribed_hf_prev_day_task >> app_opay_owealth_report_d_19_task
+ods_sqoop_base_user_hf_prev_day_task >> app_opay_owealth_report_d_19_task
+#app_opay_owealth_report_d_19_task >> send_owealth_report
 
 
