a84c76a7090b09be0aca8ba83e5e60d0e6720a25
chenlili <lili.chen@opay-inc.com>
2020-03-16 15:21:39 +0800

    新增dwm层乘客主题表

--- oride_dw_code/dwm/dwm_oride_order_base_di.py
+++ oride_dw_code/dwm/dwm_oride_order_base_di.py
@@ -368,6 +368,8 @@ def dwm_oride_order_base_di_sql_task(ds):
             --乘客端版本（发单）
             ord.client_os,
             --乘客端操作系统
+            ord.driver_version,
+            --司机端版本（接单）
             
  		   ord.country_code as country_code,
 
