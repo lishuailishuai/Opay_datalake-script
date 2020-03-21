e089c9f44c203868796a12b85efe59aa0d638844
nan.li <nan.li@opay-inc.com>
2020-03-21 20:02:32 +0800

    修改并行度

--- monitor/bussiness_alert.py
+++ monitor/bussiness_alert.py
@@ -30,7 +30,7 @@ args = {
 dag = airflow.DAG(
     'bussiness_alert',
     schedule_interval="*/10 * * * *",
-    concurrency=1,
+    concurrency=15,
     default_args=args)
 
 UTC_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
