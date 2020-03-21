b288dccaca10460d6b5e43b9b2a3f12571e7a3d1
xiedong <dong.xie@opay-inc.com>
2020-03-21 13:52:28 +0800

    修改

--- opay_dw_code/app/app_opay_user_first_trans_repurchase_30_d.py
+++ opay_dw_code/app/app_opay_user_first_trans_repurchase_30_d.py
@@ -41,17 +41,6 @@ dag = airflow.DAG('app_opay_user_first_trans_repurchase_30_d',
 
 ##----------------------------------------- 依赖 ---------------------------------------##
 
-dwd_opay_transaction_record_di_prev_day_task = OssSensor(
-    task_id='dwd_opay_transaction_record_di_prev_day_task',
-    bucket_key='{hdfs_path_str}/dt={pt}/_SUCCESS'.format(
-        hdfs_path_str="opay/opay_dw/dwd_opay_transaction_record_di/country_code=NG",
-        pt='{{ds}}'
-    ),
-    bucket_name='opay-datalake',
-    poke_interval=60,  # 依赖不满足时，一分钟检查一次依赖状态
-    dag=dag
-)
-
 
 dwm_opay_user_first_trans_repurchase_di_prev_day_task = OssSensor(
     task_id='dwm_opay_user_first_trans_repurchase_di_prev_day_task',
