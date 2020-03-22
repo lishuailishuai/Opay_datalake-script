6bb25740711565fde0ce1176cfeb4f909ef774cb
nan.li <nan.li@opay-inc.com>
2020-03-22 21:02:00 +0800

    脚本交易指标增加CQ优化

--- monitor/bussiness_alert.py
+++ monitor/bussiness_alert.py
@@ -340,7 +340,7 @@ metrcis_list = [
     # 25
     (
         'Trade_TakeRide_Success',
-        '''SELECT "trade_success_cnt" AS "trade_success_cnt"  FROM "OPAY_TRANSACTION_OP_EVENT_CQ_RESULT_10" WHERE  and time > {time} ''',
+        '''SELECT "trade_success_cnt" AS "trade_success_cnt"  FROM "OPAY_TRANSACTION_OP_EVENT_CQ_RESULT_10" WHERE  time > {time} ''',
         'trade_alert_value',
         7,
         'trade_alert_level_1_address',
