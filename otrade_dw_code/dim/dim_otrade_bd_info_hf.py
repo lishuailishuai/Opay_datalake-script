2c3e78e4abcb4b77811a66cca2e7ad3889dfbfff
yuanfeng <feng.yuan@opay-inc.com>
2020-03-25 15:25:10 +0800

    修改dag

--- otrade_dw_code/dim/dim_otrade_bd_info_hf.py
+++ otrade_dw_code/dim/dim_otrade_bd_info_hf.py
@@ -100,14 +100,21 @@ set mapred.max.split.size=1000000;
 set hive.exec.dynamic.partition.mode=nonstrict;
 set hive.exec.parallel=true;
 
---1.取出上一个小时的全量
+--1.从ods中取出最近一个分区的全量数据,bd数据是每小时全量
 with
-last_hour_total as (
+all_bd_info as (
   select
     id
     ,username
     ,mobile
     ,job_id
+    ,case
+      when job_id=1 then 'HCM'
+      when job_id=2 then 'CM'
+      when job_id=3 then 'BDM'
+      when job_id=4 then 'BD'
+      else '-'
+    end as job_name
     ,leader_id
     ,staff_id
     ,name
@@ -121,124 +128,217 @@ last_hour_total as (
     ,city_id
     ,created_at
     ,updated_at
+    ,row_number() over(partition by id order by `__ts_ms` desc,`__file` desc,cast(`__pos` as int) desc) rn
   from
     otrade_dw.dwd_otrade_bd_admin_users_hf
   where 
-    concat(dt, " ", hour) >= default.minLocalTimeRange("{config}", '{v_date}', -1) 
-    and concat(dt, " ", hour) <= default.maxLocalTimeRange("{config}", '{v_date}', -1) 
-    and utc_date_hour = from_unixtime(cast(unix_timestamp('{v_date}', 'yyyy-MM-dd HH') - 3600 as BIGINT), 'yyyy-MM-dd HH')
+    concat(dt,' ',hour) >= default.minLocalTimeRange("{config}", '{v_date}', 0)
+    and concat(dt,' ',hour) <= default.maxLocalTimeRange("{config}", '{v_date}', 0) 
+    and utc_date_hour = date_format("{v_date}", 'yyyy-MM-dd HH')
 ),
 
---2.取出上一个小时的最新增量数据
-update_info as (
+--2.分别取出每个层级的全量数据
+hcm_info as (
   select
     id
-    ,username
-    ,mobile
-    ,job_id
-    ,leader_id
-    ,staff_id
     ,name
-    ,password
+    ,job_id
+    ,job_name
+    ,mobile
+    ,username
+    ,leader_id
     ,department_id
     ,email
     ,status
-    ,remember_token
-    ,avatar
-    ,fcm_token
     ,city_id
     ,created_at
     ,updated_at
+
+    ,id as hcm_id
+    ,name as hcm_name
+    ,0 as cm_id
+    ,'-' as cm_name
+    ,0 as bdm_id
+    ,'-' as bdm_name
+    ,0 as bd_id
+    ,'-' as bd_name
   from
-    (
-    select
-      id
-      ,username
-      ,mobile
-      ,job_id
-      ,leader_id
-      ,staff_id
-      ,name
-      ,password
-      ,department_id
-      ,email
-      ,status
-      ,remember_token
-      ,avatar
-      ,fcm_token
-      ,city_id
-      ,created_at
-      ,updated_at
-      ,row_number() over(partition by id order by `__ts_ms` desc,`__file` desc,cast(`__pos` as int) desc) rn
-    from
-      otrade_dw_ods.ods_binlog_base_bd_admin_users_all_hi
-    where 
-      concat(dt, " ", hour) = date_format('{v_date}', 'yyyy-MM-dd HH') 
-      and `__deleted` = 'false'
-    ) as a
+    all_bd_info
   where
-    rn = 1
+    job_id = 1
 ),
 
---3.将如上结果集汇合
-union_result as (
+cm_info as (
+  select
+    v1.id
+    ,v1.name
+    ,v1.job_id
+    ,v1.job_name
+    ,v1.mobile
+    ,v1.username
+    ,v1.leader_id
+    ,v1.department_id
+    ,v1.email
+    ,v1.status
+    ,v1.city_id
+    ,v1.created_at
+    ,v1.updated_at
+
+    ,nvl(v2.hcm_id,0) as hcm_id
+    ,nvl(v2.hcm_name,'-') as hcm_name
+    ,v1.id as cm_id
+    ,v1.name as cm_name
+    ,0 as bdm_id
+    ,'-' as bdm_name
+    ,0 as bd_id
+    ,'-' as bd_name
+  from
+    (select * from all_bd_info where job_id = 2) as v1
+  left join
+    hcm_info as v2
+  on
+    v1.leader_id = v2.id
+),
+
+bdm_info as (
   select
     id
-    ,username
-    ,mobile
-    ,job_id
-    ,leader_id
-    ,staff_id
     ,name
-    ,password
+    ,job_id
+    ,job_name
+    ,mobile
+    ,username
+    ,leader_id
     ,department_id
     ,email
     ,status
-    ,remember_token
-    ,avatar
-    ,fcm_token
     ,city_id
     ,created_at
     ,updated_at
-    ,row_number() over(partition by id order by updated_at desc) rn
+
+    ,nvl(v2.hcm_id,0) as hcm_id
+    ,nvl(v2.hcm_name,'-') as hcm_name
+    ,nvl(v2.cm_id,0) as cm_id
+    ,nvl(v2.cm_name,'-') as cm_name
+    ,v1.id as bdm_id
+    ,v1.name as bdm_name
+    ,0 as bd_id
+    ,'-' as bd_name
   from
-    (
-    select * from last_hour_total
-    union all
-    select * from update_info
-    ) as a
+    (select * from all_bd_info where job_id = 3) as v1
+  left join
+    cm_info as v2
+  on
+    v1.leader_id = v2.id
+),
+
+bd_info as (
+  select
+    id
+    ,name
+    ,job_id
+    ,job_name
+    ,mobile
+    ,username
+    ,leader_id
+    ,department_id
+    ,email
+    ,status
+    ,city_id
+    ,created_at
+    ,updated_at
+
+    ,nvl(v2.hcm_id,0) as hcm_id
+    ,nvl(v2.hcm_name,'-') as hcm_name
+    ,nvl(v2.cm_id,0) as cm_id
+    ,nvl(v2.cm_name,'-') as cm_name
+    ,nvl(v2.bdm_id,0) as bdm_id
+    ,nvl(v2.bdm_name,'-') as bdm_name
+    ,v1.id as bd_id
+    ,v1.name as bd_name
+  from
+    (select * from all_bd_info where job_id = 4) as v1
+  left join
+    bdm_info as v2
+  on
+    v1.leader_id = v2.id
+),
+
+else_info as (
+  select
+    id
+    ,name
+    ,job_id
+    ,job_name
+    ,mobile
+    ,username
+    ,leader_id
+    ,department_id
+    ,email
+    ,status
+    ,city_id
+    ,created_at
+    ,updated_at
+
+    ,0 as hcm_id
+    ,'-' as hcm_name
+    ,0 as cm_id
+    ,'-' as cm_name
+    ,0 as bdm_id
+    ,'-' as bdm_name
+    ,0 as bd_id
+    ,'-' as bd_name
+  from
+    all_bd_info 
+  where
+    job_id = 0
+),
+
+--3.将关联好的结果融合到一起
+unoin_result as (
+  select * from hcm_info
+  union all
+  select * from cm_info
+  union all
+  select * from bdm_info
+  union all
+  select * from bd_info
+  union all
+  select * from else_info
 )
 
---4.最后将去重的结果集插入到表中
-insert overwrite table otrade_dw.dwd_otrade_bd_admin_users_hf partition(country_code,dt,hour)
-select
+--4.最后将数据插入info表中
+insert overwrite table otrade_dw.dim_otrade_bd_info_hf partition(country_code,dt,hour)
+select 
   id
-  ,username
-  ,mobile
-  ,job_id
-  ,leader_id
-  ,staff_id
   ,name
-  ,password
+  ,job_id
+  ,job_name
+  
+  ,hcm_id
+  ,hcm_name
+  ,cm_id
+  ,cm_name
+  ,bdm_id
+  ,bdm_name
+  ,bd_id
+  ,bd_name
+  
+  ,mobile
+  ,username
+  ,leader_id
   ,department_id
   ,email
   ,status
-  ,remember_token
-  ,avatar
-  ,fcm_token
   ,city_id
   ,created_at
-  ,updated_at
   ,date_format('{v_date}', 'yyyy-MM-dd HH') as utc_date_hour
 
   ,'NG' as country_code
   ,date_format(default.localTime("{config}", 'NG', '{v_date}', 0), 'yyyy-MM-dd') as dt
   ,date_format(default.localTime("{config}", 'NG', '{v_date}', 0), 'HH') as hour
 from
-  union_result
-where
-  rn = 1;
-
+  unoin_result;
 
 
 
