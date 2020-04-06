98cbdf596bc6b915e7b76eed911c2d7cb648cce4
杨明泽 <mingze.yang@opay-inc.com>
2020-04-06 09:20:39 +0800

    no message

--- opay_dw_code/ods/ods_sqoop_base_adjustment_increase_record_di.py
+++ opay_dw_code/ods/ods_sqoop_base_adjustment_increase_record_di.py
@@ -145,7 +145,7 @@ def execution_data_task_id(ds, **kargs):
     第二个参数true: 数据有才生成_SUCCESS false 数据没有也生成_SUCCESS 
 
     """
-    TaskTouchzSuccess().countries_touchz_success(ds, db_name, table_name, hdfs_path, "false", "true")
+    TaskTouchzSuccess().countries_touchz_success(ds, db_name, table_name, hdfs_path, "false", "false")
 
 
 ods_sqoop_base_adjustment_increase_record_di_task = PythonOperator(
