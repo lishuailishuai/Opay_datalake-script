5292db1c08370103314d5df73d8557e5cd973fdf
xiedong <dong.xie@opay-inc.com>
2020-04-07 18:15:48 +0800

    修改

--- opay_dw_code/ods/ods_sqoop_base_user_reseller_df.py
+++ opay_dw_code/ods/ods_sqoop_base_user_reseller_df.py
@@ -114,7 +114,7 @@ def ods_sqoop_base_user_reseller_df_sql_task(ds):
             from (
                 select 
                     *,
-                    row_number() over(partition by id order by `__ts_ms` desc,`__file` desc,cast(`__pos` as int) desc) rn
+                    row_number() over(partition by user_id order by `__ts_ms` desc,`__file` desc,cast(`__pos` as int) desc) rn
                 FROM opay_dw_ods.ods_binlog_base_user_reseller_hi
                 where concat(dt,' ',hour) between '{pt_y} 23' and '{pt} 22'
             ) m 
